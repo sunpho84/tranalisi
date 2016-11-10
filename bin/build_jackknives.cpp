acc31c2cf92db2778950c26f73ddd16bf65bc21d
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2016-11-10 16:26:03 +0100

    adding build_jackknives

--- bin/build_jackknives.cpp
+++ bin/build_jackknives.cpp
@@ -2,6 +2,19 @@
 
 int main(int narg,char **arg)
 {
-
+  input_file_t input("/tmp/input");
+  
+  //read the list of files
+  vector<pair<string,string>> file_list;
+  int nfiles=input.read<int>("NFiles");
+  for(int ifile=0;ifile<nfiles;ifile++)
+    {
+      
+      string templ_in=input.read<string>("TemplateIn");
+      string templ_out=input.read<string>("TemplateOut");
+      //cout<<input.read<string>("TemplateOut")<<endl;
+      file_list.push_back(make_pair(templ_in,templ_out));
+    }
+  
   return 0;
 }
