224e82b5467e8ee813ce1f701e6ac18c5e638d34
Lattice Group RM3 <lattice@latrm3.roma3.infn.it>
2016-11-10 18:47:33 +0100

    Fixed minuit2 installation

--- config/install_minuit2.sh
+++ config/install_minuit2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget http://www.cern.ch/mathlibs/sw/5_34_14/Minuit2/Minuit2-5.34.14.tar.gz -O -|tar xzvf -
+curl http://project-mathlibs.web.cern.ch/project-mathlibs/sw/5_34_14/Minuit2/Minuit2-5.34.14.tar.gz|tar xzvf -
 cd Minuit2-5.34.14
 mkdir build
 cd build
