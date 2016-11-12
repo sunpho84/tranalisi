51a8e405555d766c1c388ba391d86ad7923d551b
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2016-11-12 22:44:18 +0100

    adding output operator

--- lib/grace.cpp
+++ lib/grace.cpp
@@ -3,3 +3,11 @@
 #endif
 
 #include <grace.hpp>
+
+grace_file_t &operator<<(grace_file_t &out,const vec_ave_err_t &data)
+{
+  out<<"@type xydy"<<endl;
+  for(size_t i=0;i<data.size();i++) out<<i<<" "<<data[i]<<endl;
+  out.new_set();
+  return out;
+}
