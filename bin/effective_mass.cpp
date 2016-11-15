91e38c2604806f8e848cf80fd3d64a03d7620ecf
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2016-11-15 18:03:49 +0100

    fixing parsing

--- bin/effective_mass.cpp
+++ bin/effective_mass.cpp
@@ -8,20 +8,21 @@ int main(int narg,char **arg)
 {
   if(narg<4) CRASH("Use %s file T njacks iel=0 par=1",arg[0]);
   
+  //parse all pars
   const char *path=arg[1];
   int T=atoi(arg[2]);
   int ext_njacks=atoi(arg[3]);
   int iel=0;
   if(narg>=4) iel=atoi(arg[4]);
   int par=1;
-  if(narg>=5) par=atoi(arg[4]);
+  if(narg>=5) par=atoi(arg[5]);
   
   //setup the number of jackknives from command line
   set_njacks(ext_njacks);
   //read
   auto data=read_djvec(path,T,iel);
   //write average and error
-  cout<<data.symmetrized(par).ave_err()<<endl;
+  cout<<effective_mass(data.symmetrized(par)).ave_err()<<endl;
   
   return 0;
 }
