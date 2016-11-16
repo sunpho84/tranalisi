5a61a796d6c876693797c2e9f8799556965fe71d
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2016-11-16 23:49:31 +0100

    extending grace prop modifiable

--- lib/grace.hpp
+++ lib/grace.hpp
@@ -7,6 +7,13 @@
 
 using namespace std;
 
+namespace grace
+{
+  enum{WHITE,BLACK,RED,GREEN,BLUE,YELLOW,BROWN,GREY,VIOLET,CYAN,MAGENTA,ORANGE,INDIGO,MAROON,TURQUOISE,GREEN4};
+  enum{NO_SYMBOL,CIRCLE,SQUARE,DIAMOND,TRIUP,TRILEFT,TRIDOWN,TRIRIGHT,PLUS,X,STAR};
+  enum{NO_LINE,STRAIGHT_LINE};
+};
+
 //! class to write a grace file
 class grace_file_t : public ofstream
 {
@@ -31,10 +38,33 @@ public:
   
   //! set line style
   void set_line_style(size_t how){set_prop("line type "+to_string(how));}
+  void no_line(){set_line_style(grace::NO_LINE);}
   
-  //! set no line
-  void no_line(){set_line_style(0);}
-
+  //! set colors
+  void set_symbol_color(int col){set_prop("symbol color "+to_string(col));}
+  void set_symbol_fill_color(int col){set_prop("symbol fill color "+to_string(col));}
+  void set_line_color(int col){set_prop("line color "+to_string(col));}
+  void set_errorbar_color(int col){set_prop("errorbar color "+to_string(col));}
+  void set_colors(int col)
+  {
+    set_symbol_color(col);
+    set_symbol_fill_color(col);
+    set_line_color(col);
+    set_errorbar_color(col);
+  }
+  
+  //! set symbols
+  void set_symbol(int sym){set_prop("symbol "+to_string(sym));}
+  void set_no_symbol(){set_symbol(grace::NO_SYMBOL);}
+  
+  //! form a closed polygon
+  void closed_polygon(int fill_col)
+  {
+    set_prop("fill type 1");
+    set_prop("fill color "+to_string(fill_col));
+    set_line_color(fill_col);
+    set_line_style(grace::STRAIGHT_LINE);
+  }
 };
 
 //! write a vector of average and error
