e366d610007b5718a741247234924c6179cd8925
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2016-11-16 23:49:42 +0100

    put a check

--- lib/meas_vec.hpp
+++ lib/meas_vec.hpp
@@ -108,6 +108,7 @@ using dbvec_t=vmeas_t<boot_t<double>>;
 template <class T> T read_vec_meas(raw_file_t &file,size_t nel,size_t ind=0)
 {
   T out(nel);
+  if(nel==0) CRASH("Nel==0");
   file.set_pos(ind*sizeof(typename T::base_type)*out[0].size()*nel);
   out.bin_read(file);
   return out;
