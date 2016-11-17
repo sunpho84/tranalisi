b9d61ea4de1652d92a6c28ed667a1a6a09e4ba8f
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2016-11-17 11:32:18 +0100

    fixing constant fit

--- lib/fit.hpp
+++ lib/fit.hpp
@@ -14,19 +14,21 @@ using namespace std;
 using namespace ROOT::Minuit2;
 
 //! set the level of verbosity
-void set_print_level(int lev);
+inline void set_printlevel(int lev)
+{ROOT::Math::MinimizerOptions::SetDefaultPrintLevel(lev);}
 
 //! perform a fit to constant (uncorrelated)
-template <class T> T constant_fit(const vector<T> &in,int xmin,int xmax,string path="",string path_ch2="")
+template <class TV> auto constant_fit(const TV &in,int xmin,int xmax,string path="") -> typename TV::base_type
 {
+  using T=typename TV::base_type;
   T out(init_nel(in[0]));
   
   double norm=0;
   
   //take weighted average
-  for(int iel=max(xmin,0);iel<=min(xmax,in.size()-1);iel++)
+  for(int iel=max(xmin,0);iel<=min(xmax,(int)in.size()-1);iel++)
     {
-      T ele=in[iel];
+      auto ele=in[iel];
       double err=in[iel].err();
       double weight=1/(err*err);
       if(!std::isnan(err)&&err!=0)
@@ -38,7 +40,7 @@ template <class T> T constant_fit(const vector<T> &in,int xmin,int xmax,string p
   
   //take simply average if error was zero
   if(norm==0)
-    for(int iel=max(xmin,0);iel<=min(xmax,in.size()-1);iel++)
+    for(int iel=max(xmin,0);iel<=min(xmax,(int)in.size()-1);iel++)
       {
         norm++;
         out+=in[iel];
@@ -50,8 +52,11 @@ template <class T> T constant_fit(const vector<T> &in,int xmin,int xmax,string p
   if(path!="")
     {
       grace_file_t plot(path);
+      plot.write_constant_band(xmin,xmax,out,grace::RED);
+      
       plot.no_line();
-      plot<<out;
+      plot.set_colors(grace::BLACK);
+      plot<<in.ave_err();
     }
   
   return out;
