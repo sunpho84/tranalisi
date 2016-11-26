a0febb51985ab3d05dfafc4eeaecb5ab70b6e448
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2016-11-26 09:55:40 +0100

    added V0P5 P5P5
    Signed-off-by: Francesco Sanfilippo <fr.sanfilippo@gmail.com>

--- bin/Kl2_IB.cpp
+++ bin/Kl2_IB.cpp
@@ -30,14 +30,20 @@ dboot_t read_boot(const raw_file_t &file)
   return out;
 }
 
-djvec_t load(const char *path,size_t im1,size_t im2,size_t reim=RE,int rpar=1,int spat_par=1)
+djvec_t load(string path,size_t im1,size_t im2,size_t reim=RE,int rpar=1,int spat_par=1)
 {
   djvec_t corr_r0=read_djvec(path,T,ind({im1,im2,0,reim}));
   djvec_t corr_r1=read_djvec(path,T,ind({im1,im2,1,reim}));
-
-  return djvec_t(corr_r0+rpar*corr_r1).symmetrized(spat_par)*0.5;
-}
   
+  return djvec_t(corr_r0+rpar*corr_r1).symmetrized(spat_par)/(1.0+abs(rpar));
+}
+
+djvec_t load_P5P5(string ins,size_t im1,size_t im2,int reim=RE,int rpar=1,int spat_par=1)
+{return load(combine("corr%s_P5P5",ins.c_str()),im1,im2,reim,rpar,spat_par);}
+
+djvec_t load_V0P5(string ins,size_t im1,size_t im2,int reim=IM,int rpar=-1,int spat_par=-1)
+{return load(combine("corr%s_V0P5",ins.c_str()),im1,im2,reim,rpar,spat_par);}
+
 int main(int narg,char **arg)
 {
   if(narg<2) CRASH("Use %s T",arg[0]);
@@ -50,12 +56,12 @@ int main(int narg,char **arg)
   
   //pion
   
-  djvec_t pi_ratio=load("corrLL_P5P5",0,0)/load("corr00_P5P5",0,0);
+  djvec_t pi_ratio=load_P5P5("LL",0,0)/load_P5P5("00",0,0);
 
   pi_ratio.ave_err().write("pi_ratio.xmg");
 
   djack_t pi_M,pi_A,pi_SL;
-  two_pts_with_ins_ratio_fit(pi_M,pi_A,pi_SL,load("corr00_P5P5",0,0),load("corrLL_P5P5",0,0),TH,10,20,"Mpi.xmg","pi_ins.xmg");
+  two_pts_with_ins_ratio_fit(pi_M,pi_A,pi_SL,load_P5P5("00",0,0),load_P5P5("LL",0,0),TH,10,20,"Mpi.xmg","pi_ins.xmg");
   cout<<"pi_M: "<<pi_M.ave_err()<<endl;
   cout<<"pi_A: "<<pi_A.ave_err()<<endl;
   cout<<"pi_SL: "<<pi_SL.ave_err()<<endl;
@@ -69,11 +75,11 @@ int main(int narg,char **arg)
   //kaon
 
 
-  djvec_t k_ratio_exch=load("corrLL_P5P5",0,1)/load("corr00_P5P5",1,0);
-  djvec_t k_ratio_self=load("corr0M_P5P5",1,0)/load("corr00_P5P5",1,0);
-  djvec_t k_ratio_tad=load("corr0T_P5P5",1,0)/load("corr00_P5P5",1,0);
-  djvec_t k_ratio_s=load("corr0S_P5P5",1,0)/load("corr00_P5P5",1,0);
-  djvec_t k_ratio_p=load("corr0P_P5P5",1,0,IM,-1)/load("corr00_P5P5",1,0);
+  djvec_t k_ratio_exch=load_P5P5("LL",0,1)/load_P5P5("00",1,0);
+  djvec_t k_ratio_self=load_P5P5("0M",1,0)/load_P5P5("00",1,0);
+  djvec_t k_ratio_tad=load_P5P5("0T",1,0)/load_P5P5("00",1,0);
+  djvec_t k_ratio_s=load_P5P5("0S",1,0)/load_P5P5("00",1,0);
+  djvec_t k_ratio_p=load_P5P5("0P",1,0,IM,-1)/load_P5P5("00",1,0);
 
   k_ratio_exch.ave_err().write("k_ratio_exch.xmg");
   k_ratio_self.ave_err().write("k_ratio_self.xmg");
@@ -87,17 +93,17 @@ int main(int narg,char **arg)
 
   djack_t k_M,k_Z;
 
-  djvec_t corr_00=load("corr00_P5P5",1,0);
+  djvec_t corr_00=load_P5P5("00",1,0);
   two_pts_migrad_fit(k_Z,k_M,corr_00,TH,10,20,"kaon_mass.xmg");
   cout<<"k_Z: "<<k_Z.ave_err()<<endl;
   cout<<"k_M: "<<k_M.ave_err()<<endl;
 
   
   
-  two_pts_with_ins_ratio_fit(k_M1,k_A_exch,k_SL_exch,load("corr00_P5P5",1,0),load("corrLL_P5P5",0,1),TH,10,20,"Mk1.xmg","k_exch.xmg");
-  two_pts_with_ins_ratio_fit(k_M2,k_A_selftad,k_SL_selftad,load("corr00_P5P5",1,0),djvec_t(load("corr0M_P5P5",1,0)+load("corr0T_P5P5",1,0)),TH,10,20,"Mk2.xmg","k_selftad.xmg");
-  two_pts_with_ins_ratio_fit(k_M3,k_A_s,k_SL_s,load("corr00_P5P5",1,0),load("corr0S_P5P5",1,0),TH,10,20,"Mk3.xmg","k_s.xmg");
-  two_pts_with_ins_ratio_fit(k_M4,k_A_p,k_SL_p,load("corr00_P5P5",1,0),load("corr0P_P5P5",1,0),TH,10,20,"Mk4.xmg","k_p.xmg");
+  two_pts_with_ins_ratio_fit(k_M1,k_A_exch,k_SL_exch,load_P5P5("00",1,0),load_P5P5("LL",0,1),TH,10,20,"Mk1.xmg","k_exch.xmg");
+  two_pts_with_ins_ratio_fit(k_M2,k_A_selftad,k_SL_selftad,load_P5P5("00",1,0),djvec_t(load_P5P5("0M",1,0)+load_P5P5("0T",1,0)),TH,10,20,"Mk2.xmg","k_selftad.xmg");
+  two_pts_with_ins_ratio_fit(k_M3,k_A_s,k_SL_s,load_P5P5("00",1,0),load_P5P5("0S",1,0),TH,10,20,"Mk3.xmg","k_s.xmg");
+  two_pts_with_ins_ratio_fit(k_M4,k_A_p,k_SL_p,load_P5P5("00",1,0),load_P5P5("0P",1,0),TH,10,20,"Mk4.xmg","k_p.xmg");
   
   
   cout<<"k_M1: "<<k_M1.ave_err()<<endl;
@@ -116,11 +122,11 @@ int main(int narg,char **arg)
   //D meson
 
  
-  djvec_t D_ratio_exch=load("corrLL_P5P5",0,2)/load("corr00_P5P5",2,0);
-  djvec_t D_ratio_self=load("corr0M_P5P5",2,0)/load("corr00_P5P5",2,0);
-  djvec_t D_ratio_tad=load("corr0T_P5P5",2,0)/load("corr00_P5P5",2,0);
-  djvec_t D_ratio_s=load("corr0S_P5P5",2,0)/load("corr00_P5P5",2,0);
-  djvec_t D_ratio_p=load("corr0P_P5P5",2,0,IM,-1)/load("corr00_P5P5",2,0);
+  djvec_t D_ratio_exch=load_P5P5("LL",0,2)/load_P5P5("00",2,0);
+  djvec_t D_ratio_self=load_P5P5("0M",2,0)/load_P5P5("00",2,0);
+  djvec_t D_ratio_tad=load_P5P5("0T",2,0)/load_P5P5("00",2,0);
+  djvec_t D_ratio_s=load_P5P5("0S",2,0)/load_P5P5("00",2,0);
+  djvec_t D_ratio_p=load_P5P5("0P",2,0,IM,-1)/load_P5P5("00",2,0);
 
   D_ratio_exch.ave_err().write("D_ratio_exch.xmg");
   D_ratio_self.ave_err().write("D_ratio_self.xmg");
@@ -137,7 +143,7 @@ int main(int narg,char **arg)
 
   
 
-  raw_file_t file("/Users/mac/Programmi/Tesi_Laurea_Magistrale_bozza/Archivio definitivo/Tesi Magistrale/Programmi/New Nucleon/bootstrapFVE/ultimate_input.out","r");
+  raw_file_t file("ultimate_input.out","r");
 
   //int jack_index[noa][nbeta][nmusea_max][nboots];
   
@@ -205,11 +211,11 @@ int main(int narg,char **arg)
 
 
   ///////////////////////Delta m critico/////////////////////
-
-  djvec_t ward_cr=djvec_t(load("corrLL_V0P5",0,0)+load("corr0M_V0P5",0,0)+load("corr0T_V0P5",0,0)+load("corr0M_V0P5",0,0)+load("corr0T_V0P5",0,0));
+  djvec_t ward_cr=djvec_t(load_V0P5("LL",0,0)+load_V0P5("0M",0,0)+load_V0P5("0T",0,0)+load_V0P5("0M",0,0)+load_V0P5("0T",0,0));
   djvec_t num_deltam_cr=forward_derivative(ward_cr);
-  djvec_t denom_deltam_cr=forward_derivative(load("corr0P_V0P5",0,0));
-
+  num_deltam_cr.ave_err().write("num_deltam_cr.xmg");
+  djvec_t denom_deltam_cr=forward_derivative(load_V0P5("0P",0,0,RE,0,0));
+  denom_deltam_cr.ave_err().write("denom_deltam_cr.xmg");
   
   djvec_t deltam_cr=-num_deltam_cr/denom_deltam_cr;
   deltam_cr.ave_err().write("deltam_cr_t.xmg");
