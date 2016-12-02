7e826ba94489672454b9c1a44dde85613ea2de4e
Davide <Davide@MacBook-Pro-di-mac.local>
2016-12-02 13:41:20 +0100

    computing kaon corrections

--- bin/Kl2_IB_glb.cpp
+++ bin/Kl2_IB_glb.cpp
@@ -41,7 +41,7 @@ public:
 };
 /*
 template< class T> T fit_delta_pion_square()
-{return e2*pow(f0,2)*(2*par[0]/pow(f0,4)-(3+16*par[0]/pow(f0,4))*dB0*aml*ainv[ibeta]/Z[ibeta]*log(dB0*aml*ainv[ibeta]/Z[ibeta]/4)/pow(4*pi*f0,2)+par[1]*dB0*aml*ainv[ibeta]/Z[ibeta]/pow(4*pi*f0,2))+par[2]/pow(ainv[ibeta],2);}
+{return e2*pow(f0,2)*(2*par[0]/pow(f0,4)-(3+4*par[0]/pow(f0,4))*dB0*aml*ainv[ibeta]/Z[ibeta]*log(dB0*aml*ainv[ibeta]/Z[ibeta]/4)/pow(4*pi*f0,2)+par[1]*dB0*aml*ainv[ibeta]/Z[ibeta]/pow(4*pi*f0,2))+par[2]/pow(ainv[ibeta],2);}
 */
 vector<lat_par_t> lat_par(noa);
 vector<ens_data_t> raw_data;
@@ -199,26 +199,49 @@ int main(int narg,char **arg)
       dbvec_t M2Pi(raw_data.size());
       dbvec_t dM2Pi(raw_data.size());
       dbvec_t FVE_dM2Pi(raw_data.size());
+      dbvec_t dM2K_QED(raw_data.size());
+      dbvec_t FVE_dM2K(raw_data.size());
       
       for(size_t iens=0;iens<raw_data.size();iens++)
 	{
 	  size_t ens_id=raw_data[iens].iult;
 	  size_t ibeta=raw_data[iens].ibeta;
-	  boot_init_t &bi=jack_index[ia][ens_id];
 	  
 	  dboot_t a=1.0/lat_par[ia].ainv[ibeta];
+	  dboot_t L=raw_data[iens].L*a;
+	  ml[iens]=raw_data[iens].aml/lat_par[ia].Z[ibeta]/a;
+
+	  boot_init_t &bi=jack_index[ia][ens_id];
+	  
+	  dboot_t Z_QED=1.0/((sqr(ed)-sqr(eu))*e2*lat_par[ia].Z[ibeta]*(6*log(2*a)-22.596)/(32*sqr(M_PI)));
+	  dboot_t Deltam_cr_u=dboot_t(bi,raw_data[iens].deltam_cr)*e2*sqr(eu)/a;
+	  dboot_t Deltam_cr_d=dboot_t(bi,raw_data[iens].deltam_cr)*e2*sqr(ed)/a;
+
 	  dboot_t MPi=dboot_t(bi,raw_data[iens].pi_mass)/a;
 	  dboot_t MPi_sl=dboot_t(bi,raw_data[iens].pi_SL)/a;
-	  dboot_t L=raw_data[iens].L*a;
-	  
+
 	  M2Pi[iens]=MPi*MPi;
 	  dM2Pi[iens]=MPi*sqr(eu-ed)*e2*MPi_sl;
-	  ml[iens]=raw_data[iens].aml/lat_par[ia].Z[ibeta]/a;
 	  FVE_dM2Pi[iens]=FVE_d2M(MPi,L);
+	  
+	  dboot_t MK=dboot_t(bi,raw_data[iens].k_mass)/a;
+	  dboot_t MK_sl_exch=dboot_t(bi,raw_data[iens].k_SL_exch)/a;
+	  dboot_t MK_sl_selftad=dboot_t(bi,raw_data[iens].k_SL_selftad)/a;
+	  dboot_t MK_sl_s=dboot_t(bi,raw_data[iens].k_SL_s);
+	  dboot_t MK_sl_p=dboot_t(bi,raw_data[iens].k_SL_p);
+
+	  dboot_t dMK_QED=
+	    -2*ml[iens]*MK_sl_s/Z_QED
+	    -(Deltam_cr_u-Deltam_cr_d)*MK_sl_p
+	    +(sqr(eu)-sqr(ed))*e2*(MK_sl_exch-MK_sl_selftad);
+	  dM2K_QED[iens]=dMK_QED*2*MK;
+	  FVE_dM2K[iens]=FVE_d2M(MK,L);	  
 	}
       
       plot_ens_data(combine("plots/dM2Pi_an%zu.xmg",ia),ml,dM2Pi);
       plot_ens_data(combine("plots/dM2Pi_FVEcorr_an%zu.xmg",ia),ml,dM2Pi-FVE_dM2Pi);
+      plot_ens_data(combine("plots/dM2K_QED_an%zu.xmg",ia),ml,dM2K_QED);
+      plot_ens_data(combine("plots/dM2K_QED_FVEC_corr_an%zu.xmg",ia),ml,dM2K_QED-FVE_dM2K);
     }
 
   return 0;
