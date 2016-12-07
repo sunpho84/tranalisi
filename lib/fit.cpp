7c83900de7a2b4f51a906465cca51725a93d719a
Davide <Davide@MacBook-Pro-di-mac.local>
2016-12-07 19:49:15 +0100

    problems with fit terms

--- lib/fit.cpp
+++ lib/fit.cpp
@@ -7,22 +7,24 @@
 
 using namespace placeholders;
 
-template <class TL> TL fit_fsedep(const TL &L){return 1.0/(L*L*L);}
+template <class TL,class TD> TD fit_fsedep(const TL &L,const TD &D){return D/(L*L*L);}
 
 //! ansatz fit
 template <class Tpars,class Tml,class Ta>
-Tpars cont_chir_ansatz(const Tpars &f0,const Tpars &B0,const Tpars &C,const Tpars &K,const Tml &ml,const Ta &a,const Tpars &adep,double Lphys,const Tpars &D,const Tpars &A2ML,const bool chir_an)
+Tpars cont_chir_ansatz(const Tpars &f0,const Tpars &B0,const Tpars &C,const Tpars &K,const Tml &ml,const Ta &a,const Tpars &adep,double L,const Tpars &D,const Tpars &A2ML,const bool chir_an)
 {
   Tpars
-	Cf04=C/(f0*f0*f0*f0),
-	M2=2*B0*ml,
-	den=sqr(Tpars(4*M_PI*f0));
+    Cf04=C/(f0*f0*f0*f0),
+    M2=2*B0*ml,
+    den=sqr(Tpars(4*M_PI*f0));
+  // Ta
+  //   Lphys=L*a;
   
   if(chir_an)
     {
-      return e2*sqr(f0)*(2*Cf04-(3+4*Cf04)*M2/den*log(M2)+K*M2/den)+sqr(a)*adep+D/(Lphys*Lphys*Lphys)+A2ML*sqr(a)*ml;
+      return e2*sqr(f0)*(4*Cf04-(3+16*Cf04)*M2/den*log(M2)+K*M2/den)+sqr(a)*adep+fit_fsedep(L,D)+A2ML*sqr(a)*ml;
     }
-  else return e2*sqr(f0)*(2*Cf04+K*M2/den)+sqr(a)*adep+D/(Lphys*Lphys*Lphys)+A2ML*sqr(a)*ml;
+  else return e2*sqr(f0)*(4*Cf04+K*M2/den)+sqr(a)*adep+fit_fsedep(L,D)+A2ML*sqr(a)*ml;
 }
 
 void cont_chir_fit(const dbvec_t &a,const dbvec_t &z,const dboot_t &f0,const dboot_t &B0,const vector<cont_chir_fit_data_t> &ext_data,const dboot_t &ml_phys,const string &path,bool chir_an)
@@ -69,8 +71,8 @@ void cont_chir_fit(const dbvec_t &a,const dbvec_t &z,const dboot_t &f0,const dbo
   					 double a=p[2*ext_data[idata].ib+0];
   					 double z=p[2*ext_data[idata].ib+1];
   					 double ml=ext_data[idata].aml/a/z;
-					 double Lphys=ext_data[idata].L*a;
-  					 return cont_chir_ansatz(p[if0],p[iB0],p[iC],p[iK],ml,a,p[iadep],Lphys,p[iD],p[iA2ML],chir_an);
+					 double L=ext_data[idata].L;
+  					 return cont_chir_ansatz(p[if0],p[iB0],p[iC],p[iK],ml,a,p[iadep],L,p[iD],p[iA2ML],chir_an);
   				       },
   				       //error
   				       dboot_t((ext_data[idata].y-ext_data[idata].fse)/sqr(a[ext_data[idata].ib])).err()));
@@ -122,7 +124,10 @@ void cont_chir_fit(const dbvec_t &a,const dbvec_t &z,const dboot_t &f0,const dbo
   cout<<"A2ML: "<<A2ML.ave_err()<<endl;
   
   //compute physical result
-  dboot_t phys_res=cont_chir_ansatz(f0,B0,C,K,ml_phys,0.0,adep,1.0,dboot_t(D*0.0),A2ML,chir_an);
+  const double a_cont=0.0;
+  const double inf_vol=pow(10,3);
+  
+  dboot_t phys_res=cont_chir_ansatz(f0,B0,C,K,ml_phys,a_cont,adep,inf_vol,D,A2ML,chir_an);
   cout<<"Physical result: "<<phys_res.ave_err()<<endl;
   const double mpi0=0.1349766;
   const double mpip=0.13957018;
@@ -135,22 +140,11 @@ void cont_chir_fit(const dbvec_t &a,const dbvec_t &z,const dboot_t &f0,const dbo
   fit_file.set_yaxis_label("$$(M^2_{\\pi^+}-M^2_{\\pi^0}) [GeV^2]");
   fit_file.set_xaxis_max(ml_max);
 
-  vector<size_t> La={24,32,48};
   //band of the fit to individual beta
   for(size_t ib=0;ib<a.size();ib++)
-    {
-      size_t iL_min,iL_max;
-      switch(ib)
-	{
-	case 0: iL_min=0; iL_max=2; break;
-	case 1: iL_min=0; iL_max=2; break;
-	case 2: iL_min=2; iL_max=3; break;
-	}
-      for(size_t iL=iL_min;iL<iL_max;iL++)
-	fit_file.write_line(bind(cont_chir_ansatz<double,double,double>,f0.ave(),B0.ave(),C.ave(),K.ave(),_1,a[ib].ave(),adep.ave(),(La[iL]*a[ib]).ave(),D.ave(),A2ML.ave(),chir_an),1e-6,ml_max);
-    }
+    fit_file.write_line(bind(cont_chir_ansatz<double,double,double>,f0.ave(),B0.ave(),C.ave(),K.ave(),_1,a[ib].ave(),adep.ave(),inf_vol,D.ave(),A2ML.ave(),chir_an),1e-6,ml_max);
   //band of the continuum limit
-  fit_file.write_polygon(bind(cont_chir_ansatz<dboot_t,double,double>,f0,B0,C,K,_1,0.0,adep,1.0,dboot_t(D*0.0),A2ML,chir_an),1e-6,ml_max);
+  fit_file.write_polygon(bind(cont_chir_ansatz<dboot_t,double,double>,f0,B0,C,K,_1,a_cont,adep,inf_vol,D,A2ML,chir_an),1e-6,ml_max);
   //data without and with fse
   grace::default_symbol_fill_pattern=grace::FILLED_SYMBOL;
   for(int without_with_fse=0;without_with_fse<2;without_with_fse++)
@@ -164,7 +158,7 @@ void cont_chir_fit(const dbvec_t &a,const dbvec_t &z,const dboot_t &f0,const dbo
 	  for(size_t idata=0;idata<ext_data.size();idata++)
 	    if(ext_data[idata].ib==ib)
 	      fit_file<<dboot_t(ext_data[idata].aml/z[ib]/a[ib]).ave()<<" "<<
-		dboot_t((ext_data[idata].y-without_with_fse*(ext_data[idata].fse+fit_fsedep(ext_data[idata].L*a[ib])))/sqr(a[ib])).ave_err()<<endl;
+		dboot_t((ext_data[idata].y-without_with_fse*(ext_data[idata].fse+fit_fsedep(ext_data[idata].L,D)))/sqr(a[ib])).ave_err()<<endl;
 	}
       //put back colors for data with fse
       if(without_with_fse==0) fit_file.reset_cur_col();
