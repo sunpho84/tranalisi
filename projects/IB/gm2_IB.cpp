b98c733dc0d28e3041895a2140934d167f363176
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2017-02-10 14:09:19 +0100

    adding analysis

--- projects/IB/gm2_IB.cpp
+++ projects/IB/gm2_IB.cpp
@@ -3,102 +3,126 @@
 #endif
 
 #include <common.hpp>
+#include <gm2_IB_integrators.hpp>
 
-size_t T=96;
+const int ilight=0;
+size_t T=48;//96;
 size_t nm=3,nr=2;
-int ib=2;
-double a;
-double mass_muon=0.1056583745;
+int ib=0;//2;
+const int ens_id=3;
 index_t<4> ind;
+double Za=0.731;//0.762;
+boot_init_t bi;
+
+const int tmin_cr=12,tmax_cr=22;
+const int im=2;
+const double eq=ec;
 
 //! read a single vector, for a specific mass and r, real or imaginary
-djvec_t read(const char *what,size_t im,size_t r,size_t reim)
-{return read_djvec(combine("data/corr%s",what),T,ind({im,im,r,reim}));}
+dbvec_t read(const char *what,size_t im,size_t r,size_t reim)
+{return dbvec_t(bi,read_djvec(combine("data/corr%s",what),T,ind({im,im,r,reim})));}
 
 //! read a combination of r and return appropriately simmetrized
-djvec_t read(const char *what,size_t tpar,size_t im,int rpar,size_t reim)
+dbvec_t read(const char *what,int tpar,size_t im,int rpar,size_t reim)
 {
-  djvec_t o(T);
+  dbvec_t o(T);
   o=0.0;
   
-  for(size_t r=0;r<nr;r++) o+=read(what,im,r,reim);
+  for(size_t r=0;r<nr;r++) o+=read(what,im,r,reim)*(r==0?1:rpar);
   return o.symmetrized(tpar)/(1+abs(rpar));
 }
 
 //! read VV averaging the three channels
-djvec_t read_VV(const char *what,int tpar,size_t im,int rpar,size_t reim)
+dbvec_t read_VV_ren(const char *what,int tpar,size_t im,int rpar,size_t reim)
 {
-  return djvec_t(read(combine("%s_V1V1",what).c_str(),tpar,im,rpar,reim)+
-		 read(combine("%s_V2V2",what).c_str(),tpar,im,rpar,reim)+
-		 read(combine("%s_V3V3",what).c_str(),tpar,im,rpar,reim))/3.0;
+  return Za*Za*dbvec_t(read(combine("%s_V1V1",what).c_str(),tpar,im,rpar,reim)+
+		       read(combine("%s_V2V2",what).c_str(),tpar,im,rpar,reim)+
+		       read(combine("%s_V3V3",what).c_str(),tpar,im,rpar,reim))/3.0;
 }
 
-//! compute the kernel f(Q2)
-double kern_Q2(double Q2)
+//! integrate
+dboot_t integrate(const dbvec_t &corr,const dboot_t &a)
 {
-  double am=mass_muon*a;
-  cout<<"a "<<a<<" "<<am<<endl;
-  double s=Q2/sqr(am);
-  //cout<<"s "<<s<<endl;
-  double Z=(sqrt(1+4/s)-1)/2;
-  //cout<<"Z "<<Z<<endl;
-  return s*Z*Z*Z*(1-s*Z)/(1+s*Z*Z)/sqr(am);
-}
-
-//! compute the kernel f(t)
-double kern_t(size_t t)
-{
-  double out=0;
-  for(size_t iq=1;iq<T;iq++)
-    {
-      double Q=iq*2*M_PI/T;
-      //cout<<"Q "<<Q<<endl;
-      double Q2=sqr(Q);
-      out+=2*kern_Q2(Q2)*((cos(Q*t)-1)/Q2+t*t/2);
-  }
+  dboot_t out;
+  out=0.0;
+  for(size_t t=1;t<T/2-4;t++) out+=corr[t]*ftilde_t(t,a);
+  out*=4*sqr(alpha_em)*sqr(eq);
+  
   return out;
+}
+
+//! compute the critical deltam
+dboot_t compute_deltam_cr(size_t iq)
+{
+  dbvec_t V0P5_LL=read("LL_V0P5",-1,iq,-1,IM);
+  dbvec_t V0P5_0M=read("0M_V0P5",-1,iq,-1,IM);
+  dbvec_t V0P5_0T=read("0T_V0P5",-1,iq,-1,IM);
+  dbvec_t num_deltam_cr=forward_derivative(dbvec_t(V0P5_LL+2.0*dbvec_t(V0P5_0M+V0P5_0T)));
+  num_deltam_cr.ave_err().write("plots/num_deltam_cr.xmg");
+
+  dbvec_t V0P5_0P=read("0P_V0P5",-1,iq,+1,RE);
+  dbvec_t den_deltam_cr=forward_derivative(V0P5_0P);
+  den_deltam_cr.ave_err().write("plots/den_deltam_cr.xmg");
+  
+  dboot_t deltam_cr=constant_fit(dbvec_t(-num_deltam_cr/(2.0*den_deltam_cr)),tmin_cr,tmax_cr,"plots/deltam_cr_t.xmg");
+  
+  return deltam_cr;
+}
+
+//! load VV for the leading order
+dbvec_t load_LO(const int im)
+{return read_VV_ren("00",1,im,1,RE);}
+
+//! load QED corrections
+dbvec_t load_QED(const int im,const dboot_t &deltam_cr,const dbvec_t &VV_LO)
+{
+  dbvec_t VV_0T=read_VV_ren("0T",1,im,1,RE);
+  dbvec_t VV_0M=read_VV_ren("0M",1,im,1,RE);
+  dbvec_t VV_LL=read_VV_ren("LL",1,im,1,RE);
+  dbvec_t(VV_0T/VV_LO).ave_err().write("plots/VV_0T.xmg");
+  dbvec_t(VV_0M/VV_LO).ave_err().write("plots/VV_0M.xmg");
+  dbvec_t(VV_LL/VV_LO).ave_err().write("plots/VV_LL.xmg");
+  dbvec_t c=dbvec_t(VV_LL+2.0*dbvec_t(VV_0T+VV_0M));
+  
+  dbvec_t VV_0P=read_VV_ren("0P",1,im,-1,IM);
+  dbvec_t(VV_0P/VV_LO).ave_err().write("plots/VV_0P.xmg");
+  dbvec_t d=-(deltam_cr*VV_0P);
+  return dbvec_t(c+2.0*d)*e2*sqr(eq);
 }
 
 int main()
 {
   init_common_IB("/home/francesco/QCD/LAVORI/IB_MES_NF211/one_source/ultimate_input.txt");
-  a=(1/lat_par[0].ainv[0]).ave();
+  dboot_t a=1/lat_par[0].ainv[ib];
+  int input_an_id=0;
+  bi=jack_index[input_an_id][ens_id];
+  cout.precision(16);
   
-  for(size_t t=0;t<T;t++) cout<<t<<" "<<kern_t(t)<<endl;
-  return 0;
+  // for(size_t t=1;t<1000;t++) cout<<t<<" "<<(ftilde_t(t,a)/sqr(t)).ave_err()<<endl;
+  // return 0;
   ind.set_ranges({nm,nm,nr,2});
   
-  const int il=0;
-  djvec_t V0P5_LL=read("LL_V0P5",-1,il,-1,IM);
-  djvec_t V0P5_0M=read("0M_V0P5",-1,il,-1,IM);
-  djvec_t V0P5_0T=read("0T_V0P5",-1,il,-1,IM);
-  djvec_t V0P5_0P=read("0P_V0P5",-1,il,-1,RE);
+  dboot_t deltam_cr=compute_deltam_cr(ilight);
   
-  djvec_t num_deltam_cr=forward_derivative(djvec_t(V0P5_LL+2.0*djvec_t(V0P5_0M+V0P5_0T)));
-  num_deltam_cr.ave_err().write("plots/num_deltam_cr.xmg");
-  djvec_t den_deltam_cr=forward_derivative(V0P5_0P);
-  den_deltam_cr.ave_err().write("plots/den_deltam_cr.xmg");
+  dbvec_t VV_LO=load_LO(im);
+  dbvec_t VV_QED=load_QED(im,deltam_cr,VV_LO);
+  //effective_mass(VV_LO).ave_err().write("plots/VV_LO.xmg");
   
-  djvec_t deltam_cr=-num_deltam_cr/den_deltam_cr;
-  deltam_cr.ave_err().write("plots/deltam_cr_t.xmg");
+  // dbvec_t c=2.0*dbvec_t(VV_0T+VV_0M)+VV_LL;
+  // c=c.subset(0,T/2-1);
+  // dbvec_t d=deltam_cr*VV_0P.subset(0,T/2-1);
+  // c.ave_err().write("plots/c.xmg");
+  // d.ave_err().write("plots/d.xmg");
   
-  const int im=1;
-  djvec_t VV_00=read_VV("00",1,im,1,RE);
-  djvec_t VV_0P=read_VV("0P",1,im,-1,IM);
-  djvec_t VV_0T=read_VV("0T",1,im,1,RE);
-  djvec_t VV_0M=read_VV("0M",1,im,1,RE);
-  djvec_t VV_LL=read_VV("LL",1,im,1,RE);
-  effective_mass(VV_00).ave_err().write("plots/VV_00.xmg");
-  djvec_t(VV_0P/VV_00).ave_err().write("plots/VV_0P.xmg");
-  djvec_t(VV_0T/VV_00).ave_err().write("plots/VV_0T.xmg");
-  djvec_t(VV_0M/VV_00).ave_err().write("plots/VV_0M.xmg");
-  djvec_t(VV_LL/VV_00).ave_err().write("plots/VV_LL.xmg");
+  dboot_t amu=integrate(VV_LO,a);
+  cout<<"amu: "<<amu.ave_err()<<endl;
   
-  djvec_t c=2.0*djvec_t(VV_0T+VV_0M)+VV_LL;
-  c=c.subset(0,T/2-1);
-  djvec_t d=deltam_cr*VV_0P.subset(0,T/2-1);
-  c.ave_err().write("plots/c.xmg");
-  d.ave_err().write("plots/d.xmg");
+  dboot_t amu_QED=integrate(VV_QED,a);
+  cout<<"amu_QED: "<<amu_QED.ave_err()<<endl;
+  
+  cout<<" Ratio: "<<dboot_t(amu_QED/amu).ave_err()<<endl;
+  
+  close_integrators();
   
   return 0;
 }
