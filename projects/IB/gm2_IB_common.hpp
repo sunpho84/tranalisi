a6c1f47b39749fe31ca6c16012d568c5967c2298
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2017-03-10 15:11:22 +0100

    adding files

---
+++ projects/IB/gm2_IB_common.hpp
@@ -0,0 +1,182 @@
+#ifndef _GM2_IB_COMMON_HPP
+#define _GM2_IB_COMMON_HPP
+
+#include <common.hpp>
+#include <gm2_IB_integrators.hpp>
+
+const size_t ilight=0,istrange=1,icharm=2;
+const double M_V_phys[3]={0.775,1.0195,3.0969};
+size_t nm,nr;
+index_t<4> ind;
+
+const double Za_ave[nbeta]={0.731,0.737,0.762}; //M1
+const double Za_err[nbeta]={0.008,0.005,0.004};
+// double Za_ave[nbeta]={0.703,0.714,0.774}; //M2
+// double Za_err[nbeta]={0.002,0.002,0.004};
+const double Zt_ave[nbeta]={0.711,0.724,0.762}; //M1
+const double Zt_err[nbeta]={0.005,0.004,0.004};
+// double Zt_ave[nbeta]={0.700,0.711,0.767}; //M2
+// double Zt_err[nbeta]={0.003,0.002,0.002};
+const int Za_seed[nbeta]={13124,862464,76753};
+const int Zt_seed[nbeta]={5634,917453,324338};
+djvec_t Za,Zt;
+boot_init_t bi;
+
+dbvec_t alist(nbeta),zlist(nbeta);
+
+//! hold the data for a single ensemble
+class ens_data_t
+{
+public:
+  size_t iult; //< input in the ultimate file
+  size_t ib,T,L;
+  double aml;
+  string path;
+  
+  size_t tmin[3],tmax[3];
+  djack_t deltam_cr;
+};
+vector<ens_data_t> ens_data;
+size_t nens_used;
+
+//! read a single vector, for a specific mass and r, real or imaginary
+inline djvec_t read(const char *what,const ens_data_t &ens,size_t im,size_t r,size_t reim)
+{return read_djvec(combine("%s/data/corr%s",ens.path.c_str(),what),ens.T,ind({im,im,r,reim}));}
+
+//! read a combination of r and return appropriately simmetrized
+inline djvec_t read(const char *what,const ens_data_t &ens,int tpar,size_t im,int rpar,size_t reim)
+{
+  djvec_t o(ens.T);
+  o=0.0;
+  
+  for(size_t r=0;r<nr;r++) o+=read(what,ens,im,r,reim)*(r==0?1:rpar);
+  return o.symmetrized(tpar)/(1+abs(rpar));
+}
+
+//! read averaging the three channels
+inline djvec_t read(const char *what,const char *pat,const ens_data_t &ens,int tpar,size_t im,int rpar,size_t reim)
+{
+  return
+    djvec_t(read(combine("%s_%c1%c1",what,pat[0],pat[1]).c_str(),ens,tpar,im,rpar,reim)+
+	    read(combine("%s_%c2%c2",what,pat[0],pat[1]).c_str(),ens,tpar,im,rpar,reim)+
+	    read(combine("%s_%c3%c3",what,pat[0],pat[1]).c_str(),ens,tpar,im,rpar,reim))
+    /3.0;
+}
+
+//! read PP
+inline djvec_t read_PP(const char *what,const ens_data_t &ens,size_t im,int rpar,size_t reim)
+{return read(combine("%s_%s",what,"P5P5").c_str(),ens,1,im,rpar,reim);}
+
+//! read VV
+inline djvec_t read_VV(const char *what,const ens_data_t &ens,size_t im,int rpar,size_t reim)
+{return sqr(Za[ens.ib])*read(what,"VV",ens,1,im,rpar,reim);}
+
+//! read TV
+inline djvec_t read_TV(const char *what,const ens_data_t &ens,size_t im,int rpar,size_t reim)
+{return djack_t(-Za[ens.ib]*Zt[ens.ib])*read(what,"TV",ens,-1,im,rpar,reim);}
+
+//! read VT
+inline djvec_t read_VT(const char *what,const ens_data_t &ens,size_t im,int rpar,size_t reim)
+{return djack_t(Zt[ens.ib]*Za[ens.ib])*read(what,"VT",ens,-1,im,rpar,reim);}
+
+//! compute the critical deltam
+inline djack_t compute_deltam_cr(const ens_data_t &ens,size_t iq)
+{
+  djvec_t V0P5_LL=read("LL_V0P5",ens,-1,iq,-1,IM);
+  djvec_t V0P5_0M=read("0M_V0P5",ens,-1,iq,-1,IM);
+  djvec_t V0P5_0T=read("0T_V0P5",ens,-1,iq,-1,IM);
+  djvec_t num_deltam_cr=forward_derivative(djvec_t(V0P5_LL+2.0*djvec_t(V0P5_0M+V0P5_0T)));
+  num_deltam_cr.ave_err().write(combine("%s/plots/num_deltam_cr.xmg",ens.path.c_str()));
+  
+  djvec_t V0P5_0P=read("0P_V0P5",ens,-1,iq,+1,RE);
+  djvec_t den_deltam_cr=forward_derivative(V0P5_0P);
+  den_deltam_cr.ave_err().write(combine("%s/plots/den_deltam_cr.xmg",ens.path.c_str()));
+  
+  djack_t deltam_cr=constant_fit(djvec_t(-num_deltam_cr/(2.0*den_deltam_cr)),ens.tmin[iq],ens.tmax[iq],combine("%s/plots/deltam_cr_t.xmg",ens.path.c_str()));
+  
+  return deltam_cr;
+}
+
+//! read QED corrections
+inline djvec_t read_QED(const char *pat,const ens_data_t &ens,const int tpar,const int im,const djack_t &deltam_cr,const djvec_t &c_LO)
+{
+  djvec_t c_0T=read("0T",pat,ens,tpar,im,1,RE);
+  djvec_t c_0M=read("0M",pat,ens,tpar,im,1,RE);
+  djvec_t c_LL=read("LL",pat,ens,tpar,im,1,RE);
+  djvec_t(c_0T/c_LO).ave_err().write(combine("%s/plots/%s_0T.xmg",ens.path.c_str(),pat));
+  djvec_t(c_0M/c_LO).ave_err().write(combine("%s/plots/%s_0M.xmg",ens.path.c_str(),pat));
+  djvec_t(c_LL/c_LO).ave_err().write(combine("%s/plots/%s_LL.xmg",ens.path.c_str(),pat));
+  djvec_t c=djvec_t(c_LL+2.0*djvec_t(c_0T+c_0M));
+  
+  djvec_t c_0P=read("0P",pat,ens,tpar,im,-1,IM);
+  djvec_t(c_0P/c_LO).ave_err().write(combine("%s/plots/%s_0P.xmg",ens.path.c_str(),pat));
+  djvec_t d=-(deltam_cr*c_0P);
+  return djvec_t(c+2.0*d)*e2*sqr(eq[im]);
+}
+
+//! read for VV case
+inline djvec_t read_QED_VV(const ens_data_t &ens,const int tpar,const int im,const djack_t &deltam_cr,const djvec_t &c_LO)
+{return sqr(Za[ens.ib])*read_QED("VV",ens,tpar,im,deltam_cr,c_LO);}
+
+//! read for TV case
+inline djvec_t read_QED_TV(const ens_data_t &ens,const int tpar,const int im,const djack_t &deltam_cr,const djvec_t &c_LO)
+{return djack_t(Za[ens.ib]*Zt[ens.ib])*read_QED("VV",ens,tpar,im,deltam_cr,c_LO);}
+
+//! initialize gm2 calculation
+void gm2_initialize(int narg,char **arg)
+{
+  //open input file
+  string name="input_global.txt";
+  if(narg>=2) name=arg[1];
+  raw_file_t input(name,"r");
+  
+  cout.precision(16);
+  
+  //read where to read input and how many ensemble
+  string ens_pars=input.read<string>("UltimatePath");
+  nm=input.read<size_t>("NMass");
+  nr=input.read<size_t>("NR");
+  ind.set_ranges({nm,nm,nr,2});
+  init_common_IB(ens_pars);
+  nens_used=input.read<int>("NEnsemble");
+  Za.resize(nbeta);
+  Zt.resize(nbeta);
+  
+  //fill Za,Zt
+  for(size_t ibeta=0;ibeta<nbeta;ibeta++)
+    {
+      Za[ibeta].fill_gauss(Za_ave[ibeta],Za_err[ibeta],Za_seed[ibeta]);
+      Zt[ibeta].fill_gauss(Zt_ave[ibeta],Zt_err[ibeta],Zt_seed[ibeta]);
+    }
+  
+  input.expect({"Ens","beta","L","T","aml","tint_cr","tint_ss","tint_cc","path"});
+  ens_data.resize(nens_used);
+  for(size_t iens=0;iens<nens_used;iens++)
+    {
+      ens_data_t &ens=ens_data[iens];
+      
+      input.read(ens.iult);
+      input.read(ens.ib);
+      input.read(ens.L);
+      input.read(ens.T);
+      input.read(ens.aml);
+      for(size_t iq=0;iq<3;iq++)
+	{
+	  input.read(ens.tmin[iq]);
+	  input.read(ens.tmax[iq]);
+	}
+      input.read(ens.path);
+    }
+}
+
+//prepare the list of a and z
+inline void prepare_az(int input_an_id)
+{
+  for(size_t ibeta=0;ibeta<nbeta;ibeta++)
+    {
+      alist[ibeta]=1.0/lat_par[input_an_id].ainv[ibeta];
+      zlist[ibeta]=lat_par[input_an_id].Z[ibeta];
+    }
+}
+
+#endif
---
+++ projects/IB/gm2_IB_sc.cpp
@@ -0,0 +1,125 @@
+#ifdef HAVE_CONFIG_H
+ #include <config.hpp>
+#endif
+
+#include <gm2_IB_common.hpp>
+
+const int DT=4; //!< number of points to eliminate from corr
+const int im=istrange;
+
+//! ansatz fit
+template <class Tpars,class Tm,class Ta>
+Tpars cont_chir_ansatz(const Tpars &C,const Tpars &Kpi,const Tm &ml,const Ta &a,const Tpars &adep,const Tpars &adep_ml,double L,const Tpars &L2dep,const size_t an_flag)
+{return C+Kpi*ml+a*a*(adep+ml*adep_ml)+L2dep/sqr(L);}
+
+//! perform the fit to the continuum limit
+dboot_t cont_chir_fit(const dbvec_t &a,const dbvec_t &z,const vector<cont_chir_fit_data_t> &ext_data,const dboot_t &ml_phys,const string &path,size_t an_flag,bool cov_flag)
+{
+  //set_printlevel(3);
+  
+  boot_fit_t boot_fit;
+  size_t nbeta=a.size();
+  cont_chir_fit_pars_t pars(nbeta);
+  
+  ave_err_t adep_guess({6e-4,1e-4});
+  //ave_err_t adep_guess({0,0.1});
+  ave_err_t adep_ml_guess({0.0,0.0});
+  //ave_err_t adep_ml_guess({0.0,0.1});
+  pars.add_az_pars(a,z,boot_fit);
+  
+  pars.add_adep_pars(adep_guess,adep_ml_guess,boot_fit);
+  pars.iC=boot_fit.add_fit_par(pars.C,"C",1e-9,1e-9);
+  //pars.iC=boot_fit.add_fit_par(pars.C,"C",1,1e-3);
+  pars.iKPi=boot_fit.add_fit_par(pars.KPi,"KPi",0,1e-9);
+  //pars.iKPi=boot_fit.add_fit_par(pars.KPi,"KPi",0,0.1);
+  pars.iL2dep=boot_fit.add_fit_par(pars.L2dep,"L2dep",0.0,0.07);
+  
+  //boot_fit.fix_par(pars.iadep);
+  boot_fit.fix_par(pars.iL2dep);
+  //boot_fit.fix_par(pars.iKPi);
+  boot_fit.fix_par(pars.iadep_ml);
+  
+  cont_chir_fit_minimize(ext_data,pars,boot_fit,0.0,0.0,[an_flag](const vector<double> &p,const cont_chir_fit_pars_t &pars,double ml,double ms,double ac,double L)
+			 {return cont_chir_ansatz(p[pars.iC],p[pars.iKPi],ml,ac,p[pars.iadep],p[pars.iadep_ml],L,p[pars.iL2dep],an_flag);},cov_flag);
+  
+  double a_cont=0;
+  dboot_t phys_res=cont_chir_ansatz(pars.C,pars.KPi,ml_phys,a_cont,pars.adep,pars.adep_ml,inf_vol,pars.L2dep,an_flag);
+  cout<<"result: "<<phys_res.ave_err()<<endl;
+  
+  plot_chir_fit(path,ext_data,pars,
+		[&pars,an_flag]
+		(double x,size_t ib)
+		{return cont_chir_ansatz<double,double,double>
+		    (pars.C.ave(),pars.KPi.ave(),x,pars.fit_a[ib].ave(),pars.adep.ave(),pars.adep_ml.ave(),inf_vol,pars.L2dep.ave(),an_flag);},
+		bind(cont_chir_ansatz<dboot_t,double,double>,pars.C,pars.KPi,_1,a_cont,pars.adep,pars.adep_ml,inf_vol,pars.L2dep,an_flag),
+		[&ext_data,&pars]
+		(size_t idata,bool without_with_fse,size_t ib)
+		{return dboot_t(without_with_fse?ext_data[idata].wfse:ext_data[idata].wofse-without_with_fse*pars.L2dep/sqr(ext_data[idata].L));},
+		ml_phys,phys_res,"$$a_\\mu");
+  
+  return phys_res;
+}
+
+int main(int narg,char **arg)
+{
+  gm2_initialize(narg,arg);
+  
+  djvec_t LO(ens_data.size());
+  djvec_t QED(ens_data.size());
+  djvec_t ratio(ens_data.size());
+  
+  for(size_t iens=0;iens<nens_used;iens++)
+    {
+      ens_data_t &ens=ens_data[iens];
+      size_t TH=ens.T/2;
+      cout<<"----------------------- "<<iens<<" "<<ens.path<<" ---------------------"<<endl;
+      
+      djack_t deltam_cr=compute_deltam_cr(ens,ilight);
+      
+      //load LO for PP
+      djvec_t PP_LO=read_PP("00",ens,im,1,RE);
+      djack_t Z_P,M_P;
+      two_pts_fit(Z_P,M_P,PP_LO,TH,ens.tmin[im], ens.tmax[im],combine("%s/plots/PP_LO_emass.xmg",ens.path.c_str()));
+      
+      //load LO and QED for VV
+      djvec_t VV_LO=read_VV("00",ens,im,1,RE),eff_VV_LO=effective_mass(VV_LO,TH);
+      djvec_t VV_QED=read_QED_VV(ens,1,im,deltam_cr,VV_LO),VV_rat=VV_QED/VV_LO;
+      djack_t Z_V,M_V,A_V,SL_V;
+      two_pts_with_ins_ratio_fit(Z_V,M_V,A_V,SL_V,VV_LO,VV_QED,TH,ens.tmin[im],ens.tmax[im],
+				 combine("%s/plots/VV_LO.xmg",ens.path.c_str()),combine("%s/plots/VV_QED.xmg",ens.path.c_str()));
+      
+      //lattice spacing obtained from V
+      djack_t resc_a=M_V/M_V_phys[im];
+      cout<<"a: "<<resc_a.ave_err()<<endl;
+      
+      size_t upto=TH-DT;
+      
+      djack_t LO_correl=integrate_corr_up_to(VV_LO,ens.T,resc_a,im,upto);
+      djack_t LO_remaind=integrate_LO_reco_from(Z_V,M_V,resc_a,im,upto);
+      compare_LO_num_reco(combine("%s/plots/kern_LO_num_reco.xmg",ens.path.c_str()),VV_LO,Z_V,M_V,resc_a);
+      cout<<"amu: "<< LO_correl.ave_err()<<" + "<<LO_remaind.ave_err()<<endl;
+      
+      djack_t QED_correl=integrate_corr_up_to(VV_QED,ens.T,resc_a,im,upto);
+      djack_t QED_remaind=integrate_QED_reco_from(A_V,Z_V,SL_V,M_V,resc_a,im,upto);
+      compare_QED_num_reco(combine("%s/plots/kern_QED_num_reco.xmg",ens.path.c_str()),VV_QED,A_V,Z_V,SL_V,M_V,resc_a);
+      cout<<"amu_QED: "<<QED_correl.ave_err()<<" + "<<QED_remaind.ave_err()<<endl;
+      
+      // ratio[iens]=QED[iens]/LO[iens];
+      // cout<<" Ratio: "<<ratio[iens].ave_err()<<endl;
+    }
+  
+  // int input_an_id=0;
+  // prepare_az(input_an_id);
+  
+  // const size_t an_flag=0;
+  // vector<cont_chir_fit_data_t> data_LO;
+  // for(size_t iens=0;iens<ens_data.size();iens++)
+  //   {
+  //     //set bootstrap
+  //     bi=jack_index[input_an_id][ens.iult];
+  //     data_LO.push_back(cont_chir_fit_data_t(ens_data[iens].aml,ens_data[iens].aml,ens_data[iens].ib,ens_data[iens].L,ratio[iens],ratio[iens]));
+  //   }
+  // cont_chir_fit(alist,zlist,data_LO,lat_par[input_an_id].ml,combine("plots/cont_chir_LO_flag%zu_an%zu.xmg",an_flag,input_an_id,"%s"),an_flag,false);
+  
+  return 0;
+}
