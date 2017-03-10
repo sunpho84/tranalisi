57ec05fe19f4a579570dc85a7912bd1139fac089
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2017-03-10 15:12:21 +0100

    included nonperiodic

--- lib/functions.hpp
+++ lib/functions.hpp
@@ -5,32 +5,42 @@
 #include <cstdint>
 #include <vector>
 
+#include <tools.hpp>
+
 using namespace std;
 
+//! the classic 2pts correlator ansatz, with no around the world term
+template <class T> T nonperiodic_two_pts_corr_fun(T Z,T M,double t)
+{return Z*Z*exp(-M*t)/(2*M);}
 //! the classic 2pts correlator ansatz, with periodic around the world
 template <class T> T periodic_two_pts_corr_fun(T Z,T M,double TH,double t)
 {return Z*Z*exp(-M*TH)*cosh(M*(TH-t))/M;}
 //! the classic 2pts correlator ansatz, with anti-periodic around the world
-template <class T> T aperiodic_two_pts_corr_fun(T Z,T M,double TH,double t)
+template <class T> T antiperiodic_two_pts_corr_fun(T Z,T M,double TH,double t)
 {return Z*Z*exp(-M*TH)*sinh(M*(TH-t))/M;}
 //
 template <class T> T two_pts_corr_fun(T Z,T M,double TH,double t,const int par)
 {
-  if(par==1) return  periodic_two_pts_corr_fun(Z,M,TH,t);
-  else       return aperiodic_two_pts_corr_fun(Z,M,TH,t);
+  switch(par)
+    {
+    case -1: return antiperiodic_two_pts_corr_fun(Z,M,TH,t); break;
+    case  0: return nonperiodic_two_pts_corr_fun(Z, M, t);break; 
+    case +1: return  periodic_two_pts_corr_fun(Z,M,TH,t);break;
+    default: CRASH("unknown periodicity %d",par);return M;
+    }
 }
 
 //! used to solve effmass
 template <class T> T periodic_fun_effmass(T M,double TH,double t)
 {return cosh(M*(TH-t));}
-template <class T> T aperiodic_fun_effmass(T M,double TH,double t)
+template <class T> T antiperiodic_fun_effmass(T M,double TH,double t)
 {return sinh(M*(TH-t));}
 
 //! ratio of corr_fun_effmass(t+dt)/corr_fun_effmass(t)
 template <class T> T effmass_two_times_ratio_fun(T M,double TH,double t,double dt,int par)
 {
   if(par==1) return  periodic_fun_effmass(M,TH,t+dt)/ periodic_fun_effmass(M,TH,t);
-  else       return aperiodic_fun_effmass(M,TH,t+dt)/aperiodic_fun_effmass(M,TH,t);
+  else       return antiperiodic_fun_effmass(M,TH,t+dt)/antiperiodic_fun_effmass(M,TH,t);
 }
 
 //! handle keeping TH and parity and returning the function
@@ -46,16 +56,24 @@ public:
 
 ///////////////////////////////////////////////////////////////////////////////////////
 
-//! return the ratio of a corr with insertion and an original corr
+//! return the ratio of a corr with insertion and an original corr, without return contribution
+template< class T> T nonperiodic_two_pts_corr_with_ins_ratio_fun(T M,T A,T SL,double t)
+{return A+SL/M+SL*t;}
+//! return the ratio of a corr with insertion and an original corr with periodic return contribution
 template< class T> T periodic_two_pts_corr_with_ins_ratio_fun(T M,T A,T SL,double TH,double t)
-{return A-SL*(t-TH)*tanh(M*(t-TH));}
-//! return the ratio of a corr with insertion and an original corr
-template< class T> T aperiodic_two_pts_corr_with_ins_ratio_fun(T M,T A,T SL,double TH,double t)
-{return A-SL*(t-TH)/tanh(M*(t-TH));}
+{return A+SL*(1.0/M+TH)-SL*(t-TH)*tanh(M*(t-TH));}
+//! return the ratio of a corr with insertion and an original corr with antiperiodic return contribution
+template< class T> T antiperiodic_two_pts_corr_with_ins_ratio_fun(T M,T A,T SL,double TH,double t)
+{return A+SL*(1.0/M+TH)-SL*(t-TH)/tanh(M*(t-TH));}
 template <class T> T two_pts_corr_with_ins_ratio_fun(T M,T A,T SL,double TH,double t,const int par)
 {
-  if(par==1) return  periodic_two_pts_corr_with_ins_ratio_fun(M,A,SL,TH,t);
-  else       return aperiodic_two_pts_corr_with_ins_ratio_fun(M,A,SL,TH,t);
+  switch(par)
+    {
+    case -1: return antiperiodic_two_pts_corr_with_ins_ratio_fun(M,A,SL,TH,t);break;
+    case  0: return  nonperiodic_two_pts_corr_with_ins_ratio_fun(M,A,SL,t);break;
+    case +1: return     periodic_two_pts_corr_with_ins_ratio_fun(M,A,SL,TH,t);break;
+    default: CRASH("unknown periodicity %d",par);return M;
+    }
 }
 
 //! return the time dependence expected from a certain mass, of the difference of the slope at time t+dt and t
