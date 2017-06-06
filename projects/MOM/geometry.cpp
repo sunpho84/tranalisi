06195db69627b4ae3ee4d3446952e79380b77b50
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2017-06-06 16:06:52 +0200

    adding equiv moms

--- projects/MOM/geometry.cpp
+++ projects/MOM/geometry.cpp
@@ -11,6 +11,8 @@
 
 using namespace std;
 
+double ph_mom[NDIM]={0.5,0,0,0};
+
 void get_list_of_moms(const string &path)
 {
   //open the file
@@ -19,33 +21,38 @@ void get_list_of_moms(const string &path)
   do
     {
       //temporary read the coords
-      mom_t c;
+      imom_t c;
       for(auto &ci : c) mom_file>>ci;
       //if coords good, store them
-      if(mom_file.good()) moms.push_back(c);
+      if(mom_file.good()) imoms.push_back(c);
     }
   while(mom_file.good());
   
   //print stats
-  cout<<"Read "<<moms.size()<<" momenta"<<endl;
+  cout<<"Read "<<imoms.size()<<" momenta"<<endl;
 }
 
 void get_class_of_equiv_moms()
 {
-  for(size_t i=0;i<moms.size();i++)
+  for(size_t i=0;i<imoms.size();i++)
     {
       //get representative
-      mom_t cr;
-      cr[0]=moms[i][0];
+      imom_t cr;
+      
+      //decide time component
+      cr[0]=imoms[i][0];
       if(cr[0]<0) cr[0]=-cr[0]-1;
-      for(size_t mu=1;mu<NDIM;mu++) cr[mu]=abs(moms[i][mu]);
+      
+      //decide space componnents
+      for(size_t mu=1;mu<NDIM;mu++) cr[mu]=abs(imoms[i][mu]);
       sort(&cr[1],cr.end());
+      
       //store the index to equvalents
-      equiv_moms[cr].push_back(i);
+      equiv_imoms[cr].push_back(i);
     }
   
   //print stats
-  cout<<"Found "<<equiv_moms.size()<<" independent momenta "<<endl;
+  cout<<"Found "<<equiv_imoms.size()<<" independent momenta "<<endl;
   
   //print details
   // for(auto &e : equiv_moms)
@@ -59,4 +66,13 @@ void get_class_of_equiv_moms()
   // 	  cout<<endl;
   // 	}
   //   }
+}
+
+vector<double> get_indep_pt2()
+{
+  vector<double> out;
+  out.reserve(equiv_imoms.size());
+  for(auto &mom_class : equiv_imoms) out.push_back(mom_class.first.p(L).tilde().norm2());
+  
+  return out;
 }
--- projects/MOM/geometry.hpp
+++ projects/MOM/geometry.hpp
@@ -5,6 +5,8 @@
 #include <map>
 #include <vector>
 
+#include <Dirac.hpp>
+
 #ifndef EXTERN_GEOMETRY
  #define EXTERN_GEOMETRY extern
  #define INIT_TO(...)
@@ -20,26 +22,75 @@ const int NDIM=4;
 //! holds a coordinate
 using coords_t=array<int,NDIM>;
 
-//! holds a momentum - to be extended
-class mom_t : public coords_t
+//! holds a momentum
+class p_t : public array<double,NDIM>
 {
 public:
+  //! return a single component of ptilde
+  double tilde_mu(size_t mu) const
+  {return sin((*this)[mu]);}
+  
+  //! return the ptilde
+  p_t tilde()
+  {
+    p_t out;
+    for(size_t mu=0;mu<NDIM;mu++) out[mu]=tilde_mu(mu);
+    return out;
+  }
+  
+  //! return p2
+  double norm2()
+  {
+    double out=0;
+    for(size_t mu=0;mu<NDIM;mu++) out+=sqr((*this)[mu]);
+    return out;
+  }
+};
+
+//! return pslash
+inline prop_t slash(const p_t &p)
+{return Gamma[4]*p[0]+Gamma[1]*p[1]+Gamma[2]*p[2]+Gamma[3]*p[3];}
+
+//! phase of momenta
+extern double ph_mom[NDIM];
+
+//! holds the components of a momentum
+class imom_t : public coords_t
+{
+public:
+  //! return a single component of p
+  double p_mu(size_t mu,const coords_t &L) const
+  {return 2*M_PI*((*this)[mu]+ph_mom[mu])/L[mu];}
+  
+  //! return the p
+  p_t p(const coords_t &L) const
+  {
+    p_t out;
+    for(size_t mu=0;mu<NDIM;mu++) out[mu]=p_mu(mu,L);
+    return out;
+  }
 };
 
 //! lattice sizes
 EXTERN_GEOMETRY coords_t L;
 
+//! lattice volume
+EXTERN_GEOMETRY size_t V;
+
 //! list of momenta
-EXTERN_GEOMETRY vector<mom_t> moms;
+EXTERN_GEOMETRY vector<imom_t> imoms;
 
 //! reads the list of momenta from file
 void get_list_of_moms(const string &path);
 
 //! list of class of equivalence of momenta
-EXTERN_GEOMETRY map<mom_t,vector<size_t>> equiv_moms;
+EXTERN_GEOMETRY map<imom_t,vector<size_t>> equiv_imoms;
 
 //! fills the list of class of equivalence
 void get_class_of_equiv_moms();
+
+//! get a vector of pt2 for indep moms
+vector<double> get_indep_pt2();
 
 #undef EXTERN_GEOMETRY
 #undef INIT_TO
