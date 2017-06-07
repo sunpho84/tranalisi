de6137f0e30ace8f23446ae549cb1be832045e60
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2017-06-08 00:23:43 +0200

    added better handling of jacks

--- projects/MOM/mom.cpp
+++ projects/MOM/mom.cpp
@@ -10,137 +10,9 @@
 
 #include <geometry.hpp>
 
-using vprop_t=vector<vector<prop_t>>;
+using vprop_t=vector<prop_t>;
+using jverts_t=array<jprop_t,nGamma>;
 using vjprop_t=vector<jprop_t>;
-
-//! read the propagator
-vprop_t read_prop(const string &template_path,const vector<size_t> &file_list,bool verbosity=VERBOSE)
-{
-  auto start=take_time();
-  
-  vprop_t prop(file_list.size(),vector<prop_t>(imoms.size()));
-  
-#pragma omp parallel for
-  for(size_t ifile=0;ifile<file_list.size();ifile++)
-    {
-      raw_file_t file(combine(template_path.c_str(),file_list[ifile]),"r");
-      
-      if(verbosity)
-#ifdef USE_OMP
-	printf("Thread %d/%d reading file %zu/%zu\n",omp_get_thread_num(),omp_get_num_threads(),ifile,file_list.size());
-#else
-        printf("Reading file %zu/%zu\n",ifile,files.size());
-#endif
-	
-	for(size_t is_so=0;is_so<NSPIN;is_so++)
-	  for(size_t ic_so=0;ic_so<NCOL;ic_so++)
-	    for(size_t imom=0;imom<imoms.size();imom++)
-	      for(size_t is_si=0;is_si<NSPIN;is_si++)
-		for(size_t ic_si=0;ic_si<NCOL;ic_si++)
-		  file.bin_read(prop[ifile][imom](isc(is_si,ic_si),isc(is_so,ic_so)));
-    }
-  
-  cout<<"Finished reading in "<<elapsed_time(start)<<endl;
-  
-  return prop;
-}
-
-//! build the jackkniffed propagator
-vjprop_t get_jprop(const vprop_t &prop,size_t clust_size)
-{
-  auto start=take_time();
-  
-  vjprop_t jprop(imoms.size());
-  for(size_t iconf=0;iconf<prop.size();iconf++)
-#pragma omp parallel for
-    for(size_t imom=0;imom<imoms.size();imom++)
-      put_into_cluster(jprop[imom],prop[iconf][imom],iconf/clust_size);
-  for(auto &j : jprop) clusterize(j,clust_size);
-  
-  cout<<"Finished computing prop in "<<elapsed_time(start)<<endl;
-  
-  return jprop;
-}
-
-//! build the inverse
-vjprop_t get_inv_jprop(const vjprop_t &jprop)
-{
-  auto start=take_time();
-  
-  vjprop_t jprop_inv(jprop.size());
-#pragma omp parallel for
-  for(size_t imom=0;imom<imoms.size();imom++) jprop_inv[imom]=jprop[imom].inverse();
-  
-  cout<<"Finished inverting the prop in "<<elapsed_time(start)<<endl;
-  
-  return jprop_inv;
-}
-
-//! compute Zq
-djvec_t compute_Zq(const vjprop_t &jprop_inv)
-{
-  auto start=take_time();
-  
-  djvec_t out(equiv_imoms.size());
-  
-  //loop on equivalence class
-#pragma omp parallel for
-  for(size_t ind_mom=0;ind_mom<equiv_imoms.size();ind_mom++)
-    {
-      auto &imom_class=equiv_imoms[ind_mom];
-      djack_t Zq=0;
-      double pt2=imoms[imom_class.first].p(L).tilde().norm2();
-      
-      //loop on equivalent moms
-      for(size_t imom : imom_class.second)
-	{
-	  p_t ptilde=imoms[imom].p(L).tilde();
-	  prop_t pslash=slash(ptilde);
-	  Zq+=(jprop_inv[imom]*pslash.cast<cdjack_t>()).trace().imag();
-	}
-      out[ind_mom]=Zq/(12*pt2*V*imom_class.second.size());
-    }
-  
-  cout<<"Computed Zq in "<<elapsed_time(start)<<endl;
-  
-  return out;
-}
-
-//! compute projected amputated vertex on a given momentum
-djvec_t compute_projected_amputated_vertex(const vprop_t &prop,const vjprop_t &prop_inv,const prop_t &G,const prop_t &Gproj,bool ri,size_t clust_size=1)
-{
-  auto start=take_time();
-  
-  djvec_t out(equiv_imoms.size());
-  
-  //loop on equivalence class
-#pragma omp parallel for
-  for(size_t ind_mom=0;ind_mom<equiv_imoms.size();ind_mom++)
-    {
-      auto &imom_class=equiv_imoms[ind_mom];
-      out[ind_mom]=0;
-      
-      //loop on equivalent moms
-      for(size_t imom : imom_class.second)
-	{
-	  jprop_t vert;
-	  for(size_t iconf=0;iconf<prop.size();iconf++)
-	    put_into_cluster(vert,prop[iconf][imom]*G*Gamma[5]*prop[iconf][imom].adjoint()*Gamma[5],iconf/clust_size);
-	  clusterize(vert,clust_size);
-	  
-	  jprop_t amp_vert=prop_inv[imom]*vert*Gamma[5].cast<cdjack_t>()*prop_inv[imom].adjoint()*Gamma[5].cast<cdjack_t>();
-	  cdjack_t pr=(amp_vert*Gproj.cast<cdjack_t>()).trace();
-	  
-	  out[ind_mom]+=get_re_or_im(pr,ri);
-	}
-      
-      out[ind_mom]/=12*imom_class.second.size();
-    }
-  
-  cout<<"Finished computing Z in "<<elapsed_time(start)<<endl;
-  
-  return out;
-}
 
 int main(int narg,char **arg)
 {
@@ -189,21 +61,112 @@ int main(int narg,char **arg)
   vector<size_t> conf_list=get_existing_paths_in_range(template_path,conf_range);
   size_t clust_size=trim_to_njacks_multiple(conf_list,true);
   
-  //! one entry per file
-  vprop_t prop=read_prop(template_path,conf_list);
-  
   //! jackkniffed propagator
-  vjprop_t jprop=get_jprop(prop,clust_size);
+  vjprop_t jprop(imoms.size());
+  
+  //! jackkniffed vertex
+  vector<jverts_t> jverts(imoms.size());
+
+#pragma omp parallel for
+  for(size_t ijack=0;ijack<njacks;ijack++)
+    for(size_t iconf=ijack*clust_size;iconf<(ijack+1)*clust_size;iconf++)
+      {
+	string path=combine(template_path.c_str(),conf_list[iconf]);
+#ifdef USE_OMP
+	printf("Thread %d/%d reading file %zu/%zu\n",omp_get_thread_num()+1,omp_get_num_threads(),iconf+1,conf_list.size());
+#else
+	printf("Reading file %zu/%zu\n",iconf+1,conf_list.size());
+#endif
+	
+	//! source file
+	raw_file_t file(path,"r");
+	
+	//! propagator on a given conf
+	vprop_t prop(imoms.size());
+	for(size_t is_so=0;is_so<NSPIN;is_so++)
+	  for(size_t ic_so=0;ic_so<NCOL;ic_so++)
+	    for(size_t imom=0;imom<imoms.size();imom++)
+	      for(size_t is_si=0;is_si<NSPIN;is_si++)
+		for(size_t ic_si=0;ic_si<NCOL;ic_si++)
+		  file.bin_read(prop[imom](isc(is_si,ic_si),isc(is_so,ic_so)));
+	
+	for(size_t imom=0;imom<imoms.size();imom++)
+	  {
+	    // build the jackkniffed propagator
+	    put_into_cluster(jprop[imom],prop[imom],ijack);
+	    
+	    //! compute all 16 vertices
+	    for(size_t iG=0;iG<nGamma;iG++)
+	      put_into_cluster(jverts[imom][iG],prop[imom]*Gamma[iG]*Gamma[5]*prop[imom].adjoint()*Gamma[5],ijack);
+	  }
+      }
+  
+  //////////////////////////////////////////// clusterize .///////////////////////////////////////
+  
+#pragma omp parallel for
+  for(size_t imom=0;imom<imoms.size();imom++)
+    {
+      clusterize(jprop[imom]);
+      for(size_t iG=0;iG<nGamma;iG++) clusterize(jverts[imom][iG],clust_size);
+    }
+  
+  //////////////////////////////// compute the inverse propagator ////////////////////////////////
   
   //! inverse prop
-  vjprop_t jprop_inv=get_inv_jprop(jprop);
+  vjprop_t jprop_inv(jprop.size());
+#pragma omp parallel for
+  for(size_t imom=0;imom<imoms.size();imom++)
+    for(size_t ijack=0;ijack<=njacks;ijack++)
+      put_into_jackknife(jprop_inv[imom],get_from_jackknife(jprop[imom],ijack).inverse(),ijack);
   
-  //! Zq
-  djvec_t Zq=compute_Zq(jprop_inv);
+  //////////////////////////////////// compute Z by amputating //////////////////////////////////
   
-  //! compute all 16 Z
-  vector<djvec_t> Z(nGamma);
-  for(size_t iG=0;iG<nGamma;iG++) Z[iG]=compute_projected_amputated_vertex(prop,jprop_inv,Gamma[iG],Gamma[iG],RE,clust_size);
+  //! Z of quark field
+  djvec_t Zq(equiv_imoms.size());
+  
+  //! sixteen gamma Z
+  vector<djvec_t> Z(nGamma,djvec_t(equiv_imoms.size()));
+  
+#pragma omp parallel for
+  for(size_t ind_mom=0;ind_mom<equiv_imoms.size();ind_mom++)
+    {
+#ifdef USE_OMP
+      printf("Thread %d/%d analyzing mom %zu/%zu\n",omp_get_thread_num()+1,omp_get_num_threads(),ind_mom+1,equiv_imoms.size());
+#else
+      printf("Analyzing mom %zu/%zu\n",ind_mom+1,equiv_imoms.size());
+#endif
+
+      //reset Z
+      Zq[ind_mom]=0;
+      for(size_t iG=0;iG<nGamma;iG++) Z[iG][ind_mom]=0.0;
+      
+      //loop on equivalent moms
+      auto &imom_class=equiv_imoms[ind_mom];
+      double pt2=imoms[imom_class.first].p(L).tilde().norm2();
+      for(size_t imom : imom_class.second)
+	{
+	  p_t ptilde=imoms[imom].p(L).tilde();
+	  prop_t pslash=slash(ptilde);
+	  
+	  for(size_t ijack=0;ijack<=njacks;ijack++)
+	    {
+	      prop_t prop_inv=get_from_jackknife(jprop_inv[imom],ijack);
+	      Zq[ind_mom][ijack]+=(prop_inv*pslash).trace().imag();
+	      
+	      for(size_t iG=0;iG<nGamma;iG++)
+		{
+		  prop_t vert=get_from_jackknife(jverts[imom][iG],ijack) ;
+		  prop_t amp_vert=prop_inv*vert*Gamma[5]*prop_inv.adjoint()*Gamma[5];
+		  Z[iG][ind_mom][ijack]+=(amp_vert*Gamma[iG]).trace().real();
+		}
+	    }
+	}
+      
+      //normalize Z
+      Zq[ind_mom]/=12*pt2*V*imom_class.second.size();
+      for(size_t iG=0;iG<nGamma;iG++)
+	Z[iG][ind_mom]/=12*imom_class.second.size();
+    }
   
   //! normalize
   djvec_t ZS=Zq/Z[0];
