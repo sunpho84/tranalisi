0f5418984009db5c58019b7afba0d26b9c2a34b1
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2017-06-16 22:53:35 +0200

    adding sig3

---
+++ projects/MOM/sig3.cpp
@@ -0,0 +1,23 @@
+#ifdef HAVE_CONFIG_H
+ #include <config.hpp>
+#endif
+
+#ifdef USE_OMP
+ #include <omp.h>
+#endif
+
+#include <Zq.hpp>
+
+djvec_t compute_sig3(const vjprop_t &jprop_inv)
+{
+  djvec_t sigma3(imoms.size());
+    
+#pragma omp parallel for
+  for(size_t imom=0;imom<imoms.size();imom++)
+    for(size_t ijack=0;ijack<=njacks;ijack++)
+      sigma3[imom][ijack]=
+	(get_from_jackknife(jprop_inv[imom],ijack)*Gamma[5]).trace().imag()/
+	(12.0*V);
+  
+  return sigma3;
+}
