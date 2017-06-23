4a69871a595fe7ce309585e869e310b4a62bc2f1
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2017-06-24 01:34:01 +0200

    adding tmin,max

--- tools/two_pts_fit.cpp
+++ tools/two_pts_fit.cpp
@@ -8,11 +8,12 @@
 int main(int narg,char **arg)
 {
   //pars
-  int T=-1,ext_njacks=-1,iel=0,par=1,tmin,tmax;
+  int T=-1,ext_njacks=-1,iel=0,par=1,tmin=-1,tmax=-1;
   
   //parse opts
   int c;
-  switch(c)
+  while((c=getopt(narg,arg,"T:n:m:M:i::p::"))!= -1)
+    switch(c)
     {
       case 'T': T=to_int(optarg); break;
       case 'n': ext_njacks=to_int(optarg); break;
@@ -36,7 +37,8 @@ int main(int narg,char **arg)
       if(i-optind==1) path_out=arg[i];
     }
   
-  if(T==-1 or ext_njacks==-1 or path_in=="") close_with_mess("Use: %s -T=size -n=njacks -i=iel[0] -p=par[1] path_in[stdin] path_out[stdout]",arg[0]);
+  if(T==-1 or ext_njacks==-1 or path_in=="" or tmin==-1 or tmax==-1)
+    close_with_mess("Use: %s -T=size -n=njacks -i=iel[0] -m=tmin -M=tmax -p=par[1] path_in[stdin] path_out[stdout]",arg[0]);
   
   //put a warning
   if(path_in=="/dev/stdin") cerr<<"Reading from stdin"<<endl;
@@ -46,6 +48,7 @@ int main(int narg,char **arg)
   djvec_t data=read_djvec(path_in,T,iel);
   
   //write average and error
+  cout.precision(16);
   cout<<constant_fit(effective_mass(data.symmetrized(par),T/2,par),tmin,tmax,path_out)<<endl;
   
   return 0;
