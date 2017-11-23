dc88e51ef073135487f7bd96d628df3b1908eaf7
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2017-11-23 12:21:50 +0100

    adding static pow

--- lib/math.hpp
+++ lib/math.hpp
@@ -27,4 +27,42 @@ template <class T>
 T gauss_distr(double x,double x0,const T &sigma)
 {return 1/(sqrt(2*M_PI)*sigma)*exp(-sqr((x-x0)/sigma)/2.0);}
 
+//////////////////////////////// static powers
+
+namespace Impl
+{
+  //! internal implementation of power
+  template <int N>
+  struct PowN
+  {
+    template <class T>
+    static constexpr T get(T x)
+    {return x*PowN<N-1>::get(x);}
+  };
+  
+  //! special case, N=0, terminator for the recursion
+  template <>
+  struct PowN<0>
+  {
+    template <class T>
+    static constexpr T get(T x)
+    {return 1;}
+  };
+}
+
+//! static version of power
+template <int N,class T>
+constexpr T Pow(T x)
+{return Impl::PowN<N>::get(x);}
+
+//! static Square
+template <class T>
+constexpr T Sqr(T x)
+{return Pow<2>(x);}
+
+//! static Cube
+template <class T>
+constexpr T Cube(T x)
+{return Pow<3>(x);}
+
 #endif
