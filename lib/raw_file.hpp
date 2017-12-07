0d6805cbb684f3f0962b10a771c34b9ee9e06607
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2017-12-07 14:40:31 +0100

    removing debug

--- lib/raw_file.hpp
+++ lib/raw_file.hpp
@@ -34,7 +34,7 @@ public:
   void open(const string &_path,const string &mode)
   {
     path=_path;
-    cout<<"Opening "<<path<<endl;
+    //cout<<"Opening "<<path<<endl;
     file=fopen(path.c_str(),mode.c_str());
     if(file==nullptr) CRASH("Unable to open %s with mode %s",path.c_str(),mode.c_str());
   }
@@ -46,10 +46,10 @@ public:
   //! close the file
   void close()
   {
-    cout<<"Should close "<<path<<": "<<(file!=nullptr)<<endl;
+    //cout<<"Should close "<<path<<": "<<(file!=nullptr)<<endl;
     if(file!=nullptr)
       {
-	cout<<"Closing "<<path<<endl;
+	//cout<<"Closing "<<path<<endl;
 	fclose(file);
       }
     file=nullptr;
