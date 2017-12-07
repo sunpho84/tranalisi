353c5868d9776c454b02f1c8fbaba016bed7f3e2
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2017-12-07 11:11:28 +0100

    made the task not-pointer

--- lib/tasks.hpp
+++ lib/tasks.hpp
@@ -7,27 +7,38 @@
 
 using namespace std;
 
+//! incapsulate a task to be used at later stage
 using incapsulated_task_t=function<void(bool)>;
 
-const bool RECYCLE=true,DONT_RECYCLE=false;
+//! allows the task to be called again
+const bool RECYCLE=true;
+
+//! don't allow the task to be called again
+const bool DONT_RECYCLE=false;
 
 //! single task
 template<typename F,typename... Rest>
-incapsulated_task_t* incapsulate_task(F &&f,Rest&&... rest)
+incapsulated_task_t incapsulate_task(F &&f,Rest&&... rest)
 {
-  auto pck=make_shared<packaged_task<decltype(f(rest...))()>>(bind(forward<F>(f),forward<Rest>(rest)...));
-  return new function<void(bool)>([pck](bool recycle){(*pck)();if(recycle) pck->reset();});
+  auto pck=make_shared<packaged_task<decltype(f(rest...))()>>
+    (bind(forward<F>(f),forward<Rest>(rest)...));
+  return function<void(bool)>
+    ([pck](bool recycle)
+     {
+       (*pck)();
+       if(recycle) pck->reset();
+     });
 }
 
 //! list of tasks
-class task_list_t : public vector<incapsulated_task_t*>
+class task_list_t : public vector<incapsulated_task_t>
 {
 public:
-  void assolve_all(bool recycle=DONT_RECYCLE)
+  void assolve_all(const bool recycle=DONT_RECYCLE)
   {
 #pragma omp parallel for
   for(size_t itask=0;itask<this->size();itask++)
-    (*((*this)[itask]))(recycle);
+    ((*this)[itask])(recycle);
   }
 };
 
