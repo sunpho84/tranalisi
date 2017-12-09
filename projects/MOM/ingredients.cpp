6d3bf33940b61742fda305700256c37d1f7b9d9c
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2017-12-09 23:45:32 +0100

    fixed remport

--- projects/MOM/ingredients.cpp
+++ projects/MOM/ingredients.cpp
@@ -179,7 +179,7 @@ void ingredients_t::mom_compute_bil()
 	for(size_t ihit=0;ihit<nhits_to_use;ihit++)
 	  {
 	    size_t i_in_clust_ihit=i_in_clust_ihit_ind({i_in_clust,ihit});
-	    cout<<"Working on clust_entry "<<i_in_clust+1<<"/"<<clust_size<<", hit "<<ihit+1<<"/"<<nhits<<", momentum "<<ibilmom+1<<"/"<<bilmoms.size()<<endl;
+	    cout<<"Working on clust_entry "<<i_in_clust+1<<"/"<<clust_size<<", hit "<<ihit+1<<"/"<<nhits<<", momentum combo "<<ibilmom+1<<"/"<<bilmoms.size()<<endl;
 	    read_time.start();
 	    const vector<m_r_mom_conf_props_t> props1=read_all_props_mom(files,i_in_clust_ihit,imom1);
 	    const vector<m_r_mom_conf_props_t> props2=(read2)?read_all_props_mom(files,i_in_clust_ihit,imom2):props1;
