dec6b8c9240c1ccab1e4ca966e70a7cc1e3dc89b
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2018-02-22 12:36:55 +0100

    Adding print of deltam_cr

--- projects/MOM/deltam_cr.cpp
+++ projects/MOM/deltam_cr.cpp
@@ -18,19 +18,20 @@ void get_deltam_cr()
   
   //if file exists open it, otherwise compute it
   const string deltam_cr_path="deltam_cr.dat";
-  if(file_exists(deltam_cr_path)) deltam_cr.bin_read(deltam_cr_path);
+  if(file_exists(deltam_cr_path))
+    deltam_cr.bin_read(deltam_cr_path);
   else
     {
       prepare_list_of_confs();
       
       deltam_cr_time.start();
       for(size_t im=0;im<nm;im++)
-	{
-	  deltam_cr[im]=compute_deltam_cr(conf_list,tmin,tmax,im,nr,use_QED);
-	  cout<<"Deltam cr["<<im<<"]: "<<deltam_cr[im]<<endl;
-	}
+	deltam_cr[im]=compute_deltam_cr(conf_list,tmin,tmax,im,nr,use_QED);
       deltam_cr_time.stop();
       deltam_cr.bin_write(deltam_cr_path);
     }
+  
+  for(size_t im=0;im<nm;im++)
+    cout<<"Deltam cr["<<im<<"]: "<<deltam_cr[im]<<endl;
 }
 
