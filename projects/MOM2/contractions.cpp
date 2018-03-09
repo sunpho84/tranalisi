726cb78457e509d0524ac36db1e1054552e570a3
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2018-03-10 00:43:37 +0100

    Avoiding average on r

--- projects/MOM2/contractions.cpp
+++ projects/MOM2/contractions.cpp
@@ -41,7 +41,7 @@ djack_t perens_t::compute_deltam_cr(const size_t im)
     {
       djvec_t res(L[0]/2+1);
       res=0.0;
-      for(size_t r=0;r<nr;r++)
+      for(size_t r=0;r<((rdiff==0)?nr:1);r++)
 	{
 	  const string name="M"+to_string(im)+"_R"+to_string(r)+"_"+tag_bw+"_M"+to_string(im)+"_R"+to_string((r+rpar)%nr)+"_"+tag_fw;
 	  const djvec_t contr=get_contraction(name,ID,reim,tpar)*((r==0)?1:rpar)/(1+abs(rpar));
