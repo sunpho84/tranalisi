5ec8f7a1115a48a1a04f6a96b842a9fb9988cb79
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2018-03-18 01:16:04 +0100

    Adding storage of meson_mass_sea

--- projects/MOM2/meson_mass.cpp
+++ projects/MOM2/meson_mass.cpp
@@ -35,11 +35,23 @@ perens_t& perens_t::get_meson_mass()
     for(size_t im2=im1;im2<nm;im2++)
       meson_mass2_plot.write_ave_err(am[im1]+am[im2],sqr(meson_mass[im_im_ind({im1,im2})]).ave_err());
   
+  const string meson_mass_sea_path=dir_path+"/meson_mass_sea.dat";
   if(im_sea>=0 and im_sea<(int)nm) meson_mass_sea=meson_mass[im_im_ind({(size_t)im_sea,(size_t)im_sea})];
   else
+    if(file_exists(meson_mass_sea_path))
+      {
+	cout<<"File "<<meson_mass_sea_path<<" found, opening"<<endl;
+      meson_mass.bin_read(meson_mass_sea_path);
+    }
+  else
     {
+      cout<<"File "<<meson_mass_sea_path<<" not found, computing"<<endl;
+      
       prepare_list_of_confs();
+      meson_mass_time.start();
       meson_mass_sea=compute_meson_mass("sea","sea");
+      meson_mass_time.stop();
+      meson_mass.bin_write(meson_mass_sea_path);
     }
   
   return *this;
