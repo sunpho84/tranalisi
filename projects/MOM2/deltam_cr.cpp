941dd8c42a608361bad449d4b95d2f00d8d80ed8
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2018-03-23 16:03:01 +0100

    Adding symmetrized version

--- projects/MOM2/deltam_cr.cpp
+++ projects/MOM2/deltam_cr.cpp
@@ -129,6 +129,7 @@ djack_t perens_t::compute_deltam_cr(const size_t im,const size_t rfw)
       dmu.ave_err().write(dir_path+"/plots/dmu_m"+to_string(im)+"_rfw"+to_string(rfw)+".xmg");
       dmc.ave_err().write(dir_path+"/plots/dmc_m"+to_string(im)+"_rfw"+to_string(rfw)+".xmg");
       dZm.ave_err().write(dir_path+"/plots/dZm_m"+to_string(im)+"_rfw"+to_string(rfw)+".xmg");
+      dZm.symmetrized().ave_err().write(dir_path+"/plots/dZm_m"+to_string(im)+"_rfw"+to_string(rfw)+"_symm.xmg");
       
       //build numerator
       const djvec_t num_deltam_cr_corr=
