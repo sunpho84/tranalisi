d9977f97b7046d1904a046ec5f2627091f0b5951
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2018-03-28 16:38:54 +0200

    Fixed symmetric_derivative

--- projects/MOM2/deltam.cpp
+++ projects/MOM2/deltam.cpp
@@ -182,9 +182,9 @@ void perens_t::compute_deltam(const size_t im,const size_t rfw)
 	P5P5_0P+
 	P5P5_P0;
       
-      djvec_t a=symmetric_derivative(V0P5_QED)/P5P5_LO-V0P5_LO/sqr(P5P5_LO)*P5P5_QED;
-      djvec_t b=symmetric_derivative(V0P5_S)/P5P5_LO-V0P5_LO/sqr(P5P5_LO)*P5P5_S;
-      djvec_t c=symmetric_derivative(V0P5_P)/P5P5_LO-V0P5_LO/sqr(P5P5_LO)*P5P5_P;
+      djvec_t a=symmetric_derivative(V0P5_QED)/P5P5_LO-symmetric_derivative(V0P5_LO)/sqr(P5P5_LO)*P5P5_QED;
+      djvec_t b=symmetric_derivative(V0P5_S)/P5P5_LO-symmetric_derivative(V0P5_LO)/sqr(P5P5_LO)*P5P5_S;
+      djvec_t c=symmetric_derivative(V0P5_P)/P5P5_LO-symmetric_derivative(V0P5_LO)/sqr(P5P5_LO)*P5P5_P;
       djvec_t d=P5P5_QED;
       djvec_t e=P5P5_S;
       djvec_t f=P5P5_P;
