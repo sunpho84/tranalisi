4b65cc02493ac34f4e18b392b3a25fa513410548
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2018-03-29 12:05:53 +0200

    Commenting Z_LO

--- projects/MOM2/Zmeslep.cpp
+++ projects/MOM2/Zmeslep.cpp
@@ -364,7 +364,8 @@ void perens_t::compute_Zmeslep()
 		     Zq_sig1_QED[im_r_ou_ilinmom_ou][ijack]/Zq_sig1[im_r_ou_ilinmom_ou][ijack]*sqr(meslep::q_ou));
 		  
 		  auto Z_LO=Zq_contr*Gamma_meslep_combo_inv;
-		  auto Z_QED=Z_LO*(Zq_QED_contr*Zmeslep_t::Identity()-Gamma_QED_meslep_combo*Gamma_meslep_combo_inv);
+		  auto Z_QED=// Z_LO*
+		    (Zq_QED_contr*Zmeslep_t::Identity()-Gamma_QED_meslep_combo*Gamma_meslep_combo_inv);
 		  
 		  for(size_t iop=0;iop<nZop;iop++)
 		    for(size_t iproj=0;iproj<nZop;iproj++)
