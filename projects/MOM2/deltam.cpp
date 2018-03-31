bd6dd64cdfce293636717686c61eb2f982462ea5
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2018-03-31 12:05:26 +0200

    Adding old measure

--- projects/MOM2/deltam.cpp
+++ projects/MOM2/deltam.cpp
@@ -199,6 +199,9 @@ void perens_t::compute_deltam(const size_t im,const size_t rfw)
       // e.ave_err().write(dir_path+"/plots/deltam_e_m"+to_string(im)+"_rfw"+to_string(rfw)+".xmg");
       // f.ave_err().write(dir_path+"/plots/deltam_f_m"+to_string(im)+"_rfw"+to_string(rfw)+".xmg");
       
+      const djvec_t old_corr=symmetric_derivative(V0P5_QED)/symmetric_derivative(V0P5_P);
+      const djvec_t old=constant_fit(old_corr,tmin,tmax,dir_path+"/plots/old_deltam_cr_m"+to_string(im)+"_rfw"+to_string(rfw)+".xmg");
+      
       const djvec_t den=b*f-c*e;
       const djvec_t deltam_tm_corr=(-a*f+c*d)/den;
       const djvec_t deltam_cr_corr=(-b*d+a*e)/den;
