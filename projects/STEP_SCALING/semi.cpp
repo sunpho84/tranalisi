9d6457d7d7c548ad0140a93382afff465b550c86
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2018-04-10 17:49:53 +0200

    Adding 2pts

--- projects/STEP_SCALING/semi.cpp
+++ projects/STEP_SCALING/semi.cpp
@@ -4,15 +4,19 @@
 
 #include <tranalisi.hpp>
 
+const range_t range{900,100,40800};
+const size_t ntot_col=2;
+const vector<size_t> cols={0,1};
+
 int main()
 {
   set_njacks(15);
   
-  const range_t range{900,100,40800};
-  const size_t ntot_col=2;
-  const vector<size_t> cols={0,1};
+  const string template_path_2pts="out/%05d/mes_contr_2pts";
+  djvec_t data_2pts=read_conf_set_t(template_path_2pts,range,ntot_col,cols);
+  
   const string template_path_3pts="out/%05d/mes_contr_3pts";
-  read_conf_set_t(template_path_3pts,range,ntot_col,cols);
+  djvec_t data_3pts=read_conf_set_t(template_path_3pts,range,ntot_col,cols);
   
   return 0;
 }
