d4bf4e6f94da75b0c09de253f51ec7251d191e02
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2018-04-11 01:04:08 +0200

    Subtracting

--- projects/STEP_SCALING/semi.cpp
+++ projects/STEP_SCALING/semi.cpp
@@ -134,7 +134,7 @@ djvec_t get_3pts_VKP5_impr(const size_t imspec,const size_t imbw,const size_t im
 {
   int th_opp[]={0,2,1,4,3};
   
-  return (get_3pts_VKP5(imspec,imbw,imfw,ithbw,ithfw)+
+  return (get_3pts_VKP5(imspec,imbw,imfw,ithbw,ithfw)-
 	  get_3pts_VKP5(imspec,imbw,imfw,th_opp[ithbw],th_opp[ithfw]))/2.0;
 }
 
