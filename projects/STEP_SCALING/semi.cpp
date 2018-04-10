4cfd96d30034e9a6c447b14be3a0b48351aa8d59
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2018-04-10 17:43:12 +0200

    Adding semi

---
+++ projects/STEP_SCALING/semi.cpp
@@ -0,0 +1,16 @@
+#ifdef HAVE_CONFIG_H
+ #include <config.hpp>
+#endif
+
+#include <tranalisi.hpp>
+
+int main()
+{
+  const range_t range;
+  const size_t ntot_col=2;
+  const vector<size_t> cols={0,1};
+  const string template_path_3pts="out/%05d/mes_contr_3pts";
+  read_conf_set_t(template_path_3pts,range,ntot_col,cols);
+  
+  return 0;
+}
