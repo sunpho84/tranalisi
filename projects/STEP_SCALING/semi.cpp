35f4a2451d7c9fac0a3b4a1373b02aa8a46f94ac
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2018-04-10 19:06:32 +0200

    Adding back t

--- projects/STEP_SCALING/semi.cpp
+++ projects/STEP_SCALING/semi.cpp
@@ -12,7 +12,7 @@ const size_t nm=3;
 double m_list[]={0.10,0.20,0.30};
 const size_t nth=5;
 double th_list[]={0.00,-0.10,0.10,-0.20,0.20};
-const index_t i3pts_ind({{"ms",nm},{"rs",nr},{"m1",nm},{"r1",nr},{"m0",nm},{"r0",nr},{"th1",nth},{"th0",nth},{"el",nmel},{"ri",2},{"T",T}});
+const index_t i3pts_ind({{"ms",nm},{"rs",nr},{"m1",nm},{"r1",nr},{"m0",nm},{"r0",nr},{"th1",nth},{"th0",nth},{"el",nel},{"ri",2},{"T",T}});
 const string base_run="/marconi_work/INF17_lqcd123_0/sanfo/STEP_SCALING/L16_T32_beta6.61/Semileptonic/";
 
 djvec_t retrive_data(const string &binfile,const string &template_path,const index_t &ind)
@@ -20,7 +20,6 @@ djvec_t retrive_data(const string &binfile,const string &template_path,const ind
   const range_t range{900,100,40800};
   const size_t ntot_col=2;
   const vector<size_t> cols={0,1};
-  const size_t data_size=ind.max()*T;
   
   cout<<"npts: "<<ind.max()<<endl;
   
@@ -29,7 +28,7 @@ djvec_t retrive_data(const string &binfile,const string &template_path,const ind
     {
       cout<<"Binary file \""<<binfile<<"\" exists, loading"<<endl;
       
-      data.resize(data_size);
+      data.resize(ind.max());
       data.bin_read(binfile);
     }
   else
@@ -37,7 +36,7 @@ djvec_t retrive_data(const string &binfile,const string &template_path,const ind
       cout<<"Binary file \""<<binfile<<"\" does not exist, reading from txt confs, \""<<template_path<<"\""<<endl;
       
       data=read_conf_set_t(template_path,range,ntot_col,cols);
-      if(data.size()!=data_size) CRASH("loaded binary file \"%c\" data has size %zu, expected %zu",binfile.c_str(),data.size(),data_size);
+      if(data.size()!=ind.max()) CRASH("loaded binary file \"%c\" data has size %zu, expected %zu",binfile.c_str(),data.size(),ind.max());
       data.bin_write(binfile);
     }
   
@@ -46,7 +45,7 @@ djvec_t retrive_data(const string &binfile,const string &template_path,const ind
 
 djvec_t get_2pts(const size_t imbw,const size_t imfw,const size_t ithbw,const size_t ithfw,const mel_t imel)
 {
-  static const index_t i2pts_ind({{"mbw",nm},{"rbw",nr},{"mfw",nm},{"rfw",nr},{"thbw",nth},{"thfw",nth},{"mel",nmel},{"ri",2}});
+  static const index_t i2pts_ind({{"mbw",nm},{"rbw",nr},{"mfw",nm},{"rfw",nr},{"thbw",nth},{"thfw",nth},{"mel",nmel},{"ri",2},{"T",T}});
   static djvec_t data_2pts=retrive_data("2pts.dat",base_run+"out/%05d/mes_contr_2pts",i2pts_ind);
   
   int par;
@@ -66,11 +65,12 @@ djvec_t get_2pts(const size_t imbw,const size_t imfw,const size_t ithbw,const si
   
   djvec_t out(T);
   
-  for(size_t r=0;r<nr;r++)
-    {
-      size_t i=i2pts_ind({imbw,r,imfw,r,ithbw,ithfw,imel,ri});
-      out+=data_2pts.subset((i+0)*T,(i+1)*T);
-    }
+  for(size_t t=0;t<T;t++)
+    for(size_t r=0;r<nr;r++)
+      {
+	size_t i=i2pts_ind({imbw,r,imfw,r,ithbw,ithfw,imel,ri,t});
+	out[t]+=data_2pts[i];
+      }
   
   return out.symmetrized(par)/nr;
 }
