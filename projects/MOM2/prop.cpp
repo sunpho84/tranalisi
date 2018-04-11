65726a2227f752d66d6e047b5ebdfebd17c0cbf7
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2018-04-11 17:47:43 +0200

    Using only photons

--- projects/MOM2/prop.cpp
+++ projects/MOM2/prop.cpp
@@ -237,8 +237,9 @@ void perens_t::build_all_mr_jackkniffed_qprops(vector<jm_r_mom_qprops_t> &jprops
 	for(auto &jp_p : vector<tuple<jqprop_t*,const qprop_t*>>({
 	      {&j.PH,&p.FF},
 	      {&j.PH,&p.T},
-	      {&j.CR_CT,&p.P},
-	      {&j.TM_CT,&p.S}}))
+	      // {&j.CR_CT,&p.P},
+	      // {&j.TM_CT,&p.S}
+	    }))
 	  (*get<0>(jp_p))[ijack]+=*get<1>(jp_p);
     }
 }
