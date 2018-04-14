fae76288e73ebd5e6f6c675df96230f2084dfea3
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2018-04-15 00:02:07 +0200

    Adding comment

--- projects/MOM2/analysis.hpp
+++ projects/MOM2/analysis.hpp
@@ -22,6 +22,8 @@ void data_erase(const string &key);
 
 inline void compute_or_load_all()
 {
+  cout<<"Going to rotate propagators: "<<pars::twisted_run and pars::phys_basis<<endl;
+  
   for(auto &name : pars::ens)
     data(name,PRESENCE_NOT_NEEDED)
       .read_pars(name)
