253a809d9aaca6a9db0f17128d93bc5717bfe3e7
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2018-04-28 22:37:56 +0200

    Remove last change

--- projects/MOM2/sigma.cpp
+++ projects/MOM2/sigma.cpp
@@ -153,13 +153,13 @@ perens_t& perens_t::compute_sigmas()
 	  //! function to compute sigma2
 	  auto compute_sigma2=[&](const jqprop_t prop_inv)
 	    {
-	      return (prop_inv[ijack]*quaGamma[0]).trace().imag()/(12.0*V);
+	      return (prop_inv[ijack]*quaGamma[0]).trace().real()/(12.0*V);
 	    };
 	  
 	  //! function to compute sigma3
 	  auto compute_sigma3=[&](const jqprop_t prop_inv)
 	    {
-	      return (prop_inv[ijack]*quaGamma[5]).trace().imag()/(12.0*V);
+	      return (prop_inv[ijack]*quaGamma[5]).trace().real()/(12.0*V);
 	    };
 	  
 	  //! define the computation of the sigmas
