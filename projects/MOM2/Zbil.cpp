dd17229f656e8a3c192648f3955b1e8275dfb47d
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2018-06-20 18:22:15 +0200

    Adding _in_bilinears

--- projects/MOM2/Zbil.cpp
+++ projects/MOM2/Zbil.cpp
@@ -93,7 +93,7 @@ void perens_t::compute_Zbil(const bool also_QED)
 	    Zbil_QED_rel[im_r_im_r_ibil_ibilmom]=
 	      -pr_bil_QED/pr(pr_bil::LO);
 	    
-	    if(pars::include_Zq_corrections)
+	    if(pars::include_Zq_corrections_in_bilinears)
 	      Zbil_QED_rel[im_r_im_r_ibil_ibilmom]+=
 	      +(sigma1_QED_ou/s1_ou(sigma::LO)+
 		sigma1_QED_in/s1_in(sigma::LO))/2.0;
