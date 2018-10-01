cfaba501d354f4e96d6ca2f8e02a892a51f85afb
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2018-10-01 11:38:03 +0200

    Adding check

--- projects/MOM2/pr_bil.cpp
+++ projects/MOM2/pr_bil.cpp
@@ -462,6 +462,9 @@ void perens_t::assemble_pr_bil_QED_greenfunctions()
 		pr(QED_IN);
 	    
 	    cout<<pr(QED)<<" "<<pr(QED_OU)<<" "<<pr(QED_IN)<<" "<<pr(EX)<<" "<<pars::include_self_energy_in_bilinears<<" "<<QED<<" "<<QED_IN<<" "<<QED_OU<<" "<<EX<<" "<<endl;
+
+	    pr(QED)=1.0;
+	    cout<<pr(QED)<<endl;
 	    
 	    break;
 	    
