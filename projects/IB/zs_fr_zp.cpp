b4aa276abc96761a72ddf2538c8eac99cd8512fb
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2018-10-12 16:23:04 +0200

    WI determination of Zs over Zp

--- projects/IB/zs_fr_zp.cpp
+++ projects/IB/zs_fr_zp.cpp
@@ -1,7 +1,20 @@
 #include <tranalisi.hpp>
 
-const int NT=48;
-const int TH=NT/2;
+size_t ib;
+size_t NT;
+size_t TH;
+size_t tmin;
+size_t tmax;
+
+double a_GeVMOne[3]={0.4497,0.4137,0.3142};
+
+void set_NT(int ext_NT)
+{
+  NT=ext_NT;
+  TH=NT/2;
+  tmin=TH/4;
+  tmax=TH;
+}
 
 enum ins_t{_,P,S,T,F,FF};
 const string ins_tag[6]={"","_P","_S","_T","_F","_FF"};
@@ -92,6 +105,11 @@ djvec_t load(const size_t rdiff,const ins_t ins_bw,const ins_t ins_fw,const bil_
 
 int main()
 {
+  input_file_t input("analysis.txt");
+  const size_t read_NT=input.read<size_t>("T");
+  ib=input.read<size_t>("ib");
+  set_NT(read_NT);
+  input.close();
   
   set_njacks(15);
   
@@ -103,7 +121,7 @@ int main()
   const djvec_t P5P5_SAME_P=2.0*load(SAME,_,P,P5P5);
   const djvec_t P5P5_SAME_S=2.0*load(SAME,_,S,P5P5);
   const djvec_t P5P5_SAME_QED=2.0*(load(SAME,_,T,P5P5)+load(SAME,_,FF,P5P5))+load(SAME,F,F,P5P5);
-  const djvec_t eff_P5P5_LO=effective_mass(P5P5_SAME_LO);
+  const djvec_t eff_P5P5_SAME_LO=effective_mass(P5P5_SAME_LO);
   //load correlators with opposite r for P5P5
   const djvec_t P5P5_OPPO_LO=load(OPPO,_,_,P5P5);
   const djvec_t P5P5_OPPO_P=2.0*load(OPPO,_,P,P5P5);
@@ -119,9 +137,9 @@ int main()
   const djvec_t a=djvec_t(symmetric_derivative(V0P5_SAME_QED)/P5P5_SAME_LO-symmetric_derivative(V0P5_SAME_LO)/sqr(P5P5_SAME_LO)*P5P5_SAME_QED).subset(0,TH-1);
   const djvec_t b=djvec_t(symmetric_derivative(V0P5_SAME_S)/P5P5_SAME_LO-symmetric_derivative(V0P5_SAME_LO)/sqr(P5P5_SAME_LO)*P5P5_SAME_S).subset(0,TH-1);
   const djvec_t c=djvec_t(symmetric_derivative(V0P5_SAME_P)/P5P5_SAME_LO-symmetric_derivative(V0P5_SAME_LO)/sqr(P5P5_SAME_LO)*P5P5_SAME_P).subset(0,TH-1);
-  const djvec_t d=effective_slope(djvec_t(P5P5_SAME_QED/P5P5_SAME_LO),eff_P5P5_LO,TH);
-  const djvec_t e=effective_slope(djvec_t(P5P5_SAME_S/P5P5_SAME_LO),eff_P5P5_LO,TH);
-  const djvec_t f=effective_slope(djvec_t(P5P5_SAME_P/P5P5_SAME_LO),eff_P5P5_LO,TH);
+  const djvec_t d=effective_slope(djvec_t(P5P5_SAME_QED/P5P5_SAME_LO),eff_P5P5_SAME_LO,TH);
+  const djvec_t e=effective_slope(djvec_t(P5P5_SAME_S/P5P5_SAME_LO),eff_P5P5_SAME_LO,TH);
+  const djvec_t f=effective_slope(djvec_t(P5P5_SAME_P/P5P5_SAME_LO),eff_P5P5_SAME_LO,TH);
   
   //solve the system
   const djvec_t den=b*f-c*e;
@@ -129,8 +147,8 @@ int main()
   const djvec_t deltam_cr_corr=(-b*d+a*e)/den;
   
   //fit the coefficients
-  const djack_t deltam_cr=constant_fit(deltam_cr_corr,TH/4,TH*3/4,"plots/deltam_cr_corr_SAME.xmg");
-  const djack_t deltam_tm=constant_fit(deltam_tm_corr,TH/4,TH*3/4,"plots/deltam_tm_corr_SAME.xmg");
+  const djack_t deltam_cr=constant_fit(deltam_cr_corr,tmin,tmax,"plots/deltam_cr_corr_SAME.xmg");
+  const djack_t deltam_tm=constant_fit(deltam_tm_corr,tmin,tmax,"plots/deltam_tm_corr_SAME.xmg");
   cout<<"deltam_cr: "<<smart_print(deltam_cr.ave_err())<<endl;
   cout<<"deltam_tm: "<<smart_print(deltam_tm.ave_err())<<endl;
   
@@ -138,16 +156,28 @@ int main()
   const djvec_t P5P5_SAME_CORR=P5P5_SAME_QED+deltam_tm*P5P5_SAME_S+deltam_cr*P5P5_SAME_P;
   const djvec_t P5P5_SAME_REL_CORR=P5P5_SAME_CORR/P5P5_SAME_LO;
   P5P5_SAME_REL_CORR.ave_err().write("plots/P5P5_SAME_CORR.xmg");
+  djack_t G2P_SAME,M_SAME,DG2_FR_G2_SAME,M_SL_SAME;
+  two_pts_with_ins_ratio_fit(G2P_SAME,M_SAME,DG2_FR_G2_SAME,M_SL_SAME,P5P5_SAME_LO,P5P5_SAME_CORR,TH,tmin,tmax,"plots/P5P5_SAME_effmass.xmg","plots/P5P5_SAME_CORR_eff_slope.xmg");
+  const djack_t DG2_FR_G2_SAME_constfit=constant_fit(P5P5_SAME_REL_CORR,tmin,tmax,"plots/P5P5_SAME_CORR_constant_fit.xmg");
   
   //build corrected correlators with opposite r
   const djvec_t P5P5_OPPO_CORR=P5P5_OPPO_QED+deltam_tm*P5P5_OPPO_S+deltam_cr*P5P5_OPPO_P;
   const djvec_t P5P5_OPPO_REL_CORR=P5P5_OPPO_CORR/P5P5_OPPO_LO;
   P5P5_OPPO_REL_CORR.ave_err().write("plots/P5P5_OPPO_CORR.xmg");
+  djack_t G2P_OPPO,M_OPPO,DG2_FR_G2_OPPO,M_SL_OPPO;
+  two_pts_with_ins_ratio_fit(G2P_OPPO,M_OPPO,DG2_FR_G2_OPPO,M_SL_OPPO,P5P5_OPPO_LO,P5P5_OPPO_CORR,TH,tmin,tmax,"plots/P5P5_OPPO_effmass.xmg","plots/P5P5_OPPO_CORR_eff_slope.xmg");
   
   //build the correlator representing the correction of Zs/Zp
-  const djvec_t dZs_fr_Zp_corr=(P5P5_SAME_REL_CORR-P5P5_OPPO_REL_CORR)/2.0;
-  const djack_t dZs_fr_Zp=constant_fit(dZs_fr_Zp_corr,TH/4,TH*3/4,"plots/dZs_fr_Zp.xmg");
-  cout<<"dZs_fr_Zp: "<<smart_print(dZs_fr_Zp)<<endl;
+  // const djvec_t dZs_fr_Zp_corr=(P5P5_SAME_REL_CORR-P5P5_OPPO_REL_CORR)/2.0;
+  // const djack_t dZs_fr_Zp=constant_fit(dZs_fr_Zp_corr,tmin,tmax,"plots/dZs_fr_Zp.xmg");
+  // cout<<"dZs_fr_Zp rough: "<<smart_print(dZs_fr_Zp)<<endl;
+  
+  const djack_t dZs_fr_Zp_fitted=(DG2_FR_G2_SAME-DG2_FR_G2_OPPO)/2.0;
+  cout<<"dZs_fr_Zp fitted: "<<smart_print(dZs_fr_Zp_fitted)<<endl;
+  cout<<M_SAME.ave()/a_GeVMOne[ib]<<" "<<dZs_fr_Zp_fitted<<endl;
+  
+  // const djack_t dZs_fr_Zp_fitted_with_same_const=(DG2_FR_G2_SAME_constfit-DG2_FR_G2_OPPO)/2.0;
+  // cout<<"dZs_fr_Zp fitted with \"same\" constant: "<<smart_print(dZs_fr_Zp_fitted_with_same_const)<<endl;
   
   return 0;
 }
