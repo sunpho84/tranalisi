a31c8d97421ca26208c424943df448b12d45e4fa
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2019-02-14 11:46:25 +0100

    Added reading

--- projects/IB/disco2.cpp
+++ projects/IB/disco2.cpp
@@ -1,8 +1,56 @@
 #include <tranalisi.hpp>
 
-int main()
+int T,L;
+
+vector<complex<double>> read_vector(const string &path,int n)
 {
-  set_njacks(15);
+  raw_file_t data(path,"r");
+  
+  vector<complex<double>> d;
+  
+  for(int i=0;i<n;i++)
+    d.emplace_back(data.read<double>(),data.read<double>());
+  
+  return d;
+}
+
+int main(int narg,char **arg)
+{
+  string name="input.txt";
+  if(narg>=2) name=arg[1];
+  raw_file_t input(name,"r");
+  
+  cout.precision(16);
+  
+  T=input.read<int>("T");
+  L=input.read<int>("L");
+  range_t conf_range;
+  input.expect("ConfRange");
+  conf_range.start=input.read<size_t>();
+  conf_range.each=input.read<size_t>();
+  conf_range.end=input.read<size_t>();
+  
+  set_njacks(input.read<int>("NJacks"));
+  
+  const int nposs_confs=(conf_range.end-conf_range.start)/conf_range.each;
+  cout<<"Npossible confs: "<<nposs_confs<<endl;
+  const int clust_size=nposs_confs/njacks;
+  cout<<"Cluster size: "<<clust_size<<endl;
+  const int nconfs=clust_size*njacks;
+  cout<<"Nconfs: "<<nconfs<<endl;
+  
+  const int nhits=input.read<int>("NHits");
+  
+  for(int iconf=0;iconf<nconfs;iconf++)
+    {
+      const int conf=iconf*conf_range.each+conf_range.start;
+      vector<complex<double>> EU1_stoch=read_vector(combine("%04d/EU1_stoch",conf),nhits);
+      vector<complex<double>> EU2_stoch=read_vector(combine("%04d/EU2_stoch",conf),nhits);
+      vector<complex<double>> EU4_stoch=read_vector(combine("%04d/EU4_stoch",conf),nhits);
+      vector<complex<double>> EU5_stoch=read_vector(combine("%04d/EU5_stoch",conf),nhits*(nhits-1)/2);
+      vector<complex<double>> EU6_stoch=read_vector(combine("%04d/EU6_stoch",conf),nhits*(nhits-1)/2);
+      
+    }
   
   return 0;
 }
