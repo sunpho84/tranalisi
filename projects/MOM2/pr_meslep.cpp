ada44418df557d38f10c1ff4b1963b28e518f544
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2019-03-04 17:07:21 +0100

    Printing x

--- projects/MOM2/pr_meslep.cpp
+++ projects/MOM2/pr_meslep.cpp
@@ -515,7 +515,7 @@ void perens_t::val_chir_extrap_pr_meslep(perens_t &out) const
 		  {
 		    cout<<" ijack: "<<ijack<<endl;
 		    for(int ipoint=0;ipoint<y_plot.size();ipoint++)
-		      cout<<" "<<ipoint<<y_plot[ipoint][ijack]<<" "<<y_plot[ipoint].err()<<endl;
+		      cout<<" "<<x[ipoint]<<" "<<y_plot[ipoint][ijack]<<" "<<y_plot[ipoint].err()<<endl;
 		  }
 		    
 		write_fit_plot(*plot,xmin,xmax,[&coeffs,sub_pole,meslepins](double x)->djack_t{return poly_eval<djvec_t>(coeffs[meslepins],x)/(sub_pole?x:1);},x,y_plot);
