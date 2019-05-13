d17d35e86a4cabf9fdf615e2364449d5ff21a8db
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2019-05-13 18:48:45 +0200

    Adding a base reader

--- projects/REPH/reph.cpp
+++ projects/REPH/reph.cpp
@@ -2,7 +2,15 @@
 
 int main(int narg,char **arg)
 {
+  set_njacks(15);
   
+  const int T=48;
+  
+  djvec_t corr(T);
+  
+  corr.bin_read("jacks/oPPo-ss");
+  
+  cout<<corr.ave_err()<<endl;
   
   return 0;
 }
