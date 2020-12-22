13780890a1f93bb81f2a63adbcbee6631ad69b62
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2020-12-22 23:09:19 +0100

    Fitter working

--- projects/fitter/fitter.cpp
+++ projects/fitter/fitter.cpp
@@ -13,46 +13,157 @@
 
 using namespace std;
 
-vector<double> pars;
+node_t* parseFormula(const string& formulaString);
 
-void init_scanner();
-void destroy_scanner();
-// int main()
-// {
-  
-//   //   real val(10);
-//   // uexp e(exp,&val);
-//   // bexp s(sum,&val,&e);
-//   // uexp ss(sin,&e);
-//   // cout<<ss.eval()<<endl;
-  
-//   return 0;
-// }
+const size_t nArgsPerFile=5;
 
-int i=0;
-string str("8*(8--7)");
-
-void fitter_lex_internal(char* buf,int &result,int max_size)
+///Exit with help message
+void exitWithHelp(const char* execName)
 {
-  result=(str[i]!='\0');
-  *buf=str[i++];
+  CRASH("Use %s fileData xmin xmax formula filePlot [, fileData xmin xmax formula filePlot, ... ] [ -- ansatz ]",execName);
 }
 
-int main(int narg,char **arg)
+/// Data
+struct data_fit_t
 {
-  if(narg<2)
-    CRASH("Use %s formula",arg[0]);
+  const string fileData;
+  const double xmin,xmax;
+  const node_t* formula;
+  const string filePlot;
   
-  for(int iarg=1;iarg<narg;iarg++)
+  vector<array<double,3>> data;
+  
+  data_fit_t(const string& fileData,const double xmin,const double xmax,const string& formulaString,const string& filePlot) :
+    fileData(fileData),xmin(xmin),xmax(xmax),formula(parseFormula(formulaString)),filePlot(filePlot)
+  {
+    /// Open the file
+    obs_file_t fin(fileData,3,{0,1,2});
+    
+    /// Count the lines
+    const size_t nLines=fin.length(1)/3;
+    data.resize(nLines);
+    
+    for(size_t iLine=0;iLine<nLines;iLine++)
+      {
+	/// Single line
+	const vector<double> l=fin.read(1);
+	for(size_t i=0;i<3;i++) data[iLine][i]=l[i];
+      }
+  }
+};
+
+vector<data_fit_t> dataFit;
+vector<size_t> parIds;
+map<size_t,size_t> idOfPars;
+vector<double> guessPars;
+
+/// Parse all arguments
+void parseArgs(int narg,char** arg)
+{
+  int iDivAnsatz=0;
+  while(iDivAnsatz<narg and strcasecmp(arg[iDivAnsatz],"--"))
+    iDivAnsatz++;
+  cout<<"DivAnsatz: "<<iDivAnsatz<<endl;
+  
+  /// Number of files to be fit
+  const int nFiles=(iDivAnsatz-1)/nArgsPerFile;
+  if(iDivAnsatz!=(int)(1+nFiles*nArgsPerFile))
+    exitWithHelp(arg[0]);
+  cout<<"NFiles: "<<nFiles<<endl;
+  if(nFiles<1)
+    CRASH("Please provide at least one file to fit");
+  
+  set<size_t> parIdsSet;
+  for(int iFile=0;iFile<nFiles;iFile++)
     {
-      str=arg[iarg];
-      i=0;
+      enum{PathData=0,Xmin=1,Xmax=2,Formula=3,PathPlot=4};
+      char** pars=arg+1+iFile*nArgsPerFile;
       
-      init_scanner();
+      const string pathData=pars[PathData];
+      const double xMin=strtod(pars[Xmin],nullptr);
+      const double xMax=strtod(pars[Xmax],nullptr);
+      const string formula=pars[Formula];
+      const string pathPlot=pars[PathPlot];
       
-      fitter_parser_parse(nullptr);
+      dataFit.emplace_back(pathData,xMin,xMax,formula,pathPlot);
       
-      destroy_scanner();
+      for(const size_t& i : dataFit.back().formula->getParIds())
+	parIdsSet.insert(i);
+    }
+  
+  parIds.reserve(parIdsSet.size());
+  for(auto& i : parIdsSet)
+    {
+      idOfPars[i]=parIds.size();
+      parIds.push_back(i);
+    }
+  
+  for(size_t iArg=iDivAnsatz+1;iArg<(size_t)narg;iArg++)
+    guessPars.push_back(strtod(arg[iArg],nullptr));
+}
+
+//! perform a simple fit using x, a function and data
+class ch2_t : public minimizer_fun_t
+{
+public:
+  //! constructor
+  ch2_t()
+  {
+  }
+  
+  //! compute the function
+  double operator()(const vector<double> &p) const
+  {
+    double ch2=0;
+    
+    for(auto& d : dataFit)
+      for(const auto& c : d.data)
+	{
+	  const double& x=c[0];
+	  const double& n=c[1];
+	  const double& e=c[2];
+	  const double t=d.formula->eval(p,x);
+	  double contr=sqr((n-t)/e);
+	  // cout<<contr<<" = [("<<n<<"-f("<<x<<")="<<t<<")/"<<e<<"]^2]"<<endl;
+	  if(x>=d.xmin and x<=d.xmax)
+	    ch2+=contr;
+	}
+    
+    // cout<<"ch2: "<<ch2<<endl;
+    
+    return ch2;
+  }
+  
+  double Up() const {return 1;}
+};
+
+int main(int narg,char** arg)
+{
+  parseArgs(narg,arg);
+  
+  const size_t nPars=parIds.size();
+  cout<<"NPars: "<<nPars<<endl;
+  guessPars.resize(nPars);
+  
+  minimizer_pars_t fitPars;
+  for(size_t iPar=0;iPar<nPars;iPar++)
+    fitPars.add(combine("[%zu]",parIds[iPar]),guessPars[iPar],fabs(guessPars[iPar]/10));
+  
+  ch2_t ch2;
+  
+  minimizer_t minimizer(ch2,fitPars);
+  
+  vector<double> pars=minimizer.minimize();
+  cout<<pars<<endl;
+  
+  for(auto& data : dataFit)
+    {
+      grace_file_t plot(data.filePlot);
+      
+      for(auto& d : data.data)
+	plot.write_ave_err(d[0],{d[1],d[2]});
+      
+      plot.write_line([&data,&pars](const double& x){return data.formula->eval(pars,x);},data.xmin,data.xmax,grace::RED);
     }
   
   return 0;
--- projects/fitter/node.hpp
+++ projects/fitter/node.hpp
@@ -2,19 +2,28 @@
 #define _NODE_HPP
 
 #include <vector>
+#include <ostream>
+#include <set>
+#include <sstream>
+
+#include <tranalisi.hpp>
 
 using namespace std;
-
-extern vector<double> pars;
 
 /// Generic node
 struct node_t
 {
   /// Virtual evaluator
-  virtual double eval() const = 0;
+  virtual double eval(const vector<double>& pars,const double& x) const = 0;
   
   /// Virtual destructor
   virtual ~node_t() = 0;
+  
+  /// Print to an ostream
+  virtual ostream& print(ostream&) const = 0;
+  
+  /// Get parameters
+  virtual set<size_t> getParIds() const = 0;
 };
 
 /// Virtual destructor
@@ -22,14 +31,50 @@ inline node_t::~node_t()
 {
 }
 
+/////////////////////////////////////////////////////////////////
+
+/// Variable
+struct variable_node_t : public node_t
+{
+  /// Creates
+  variable_node_t()
+  {
+  }
+  
+  /// Destruct
+  ~variable_node_t()
+  {
+  }
+  
+  /// Evaluate
+  double eval(const vector<double>& pars,const double& x) const
+  {
+    return x;
+  }
+  
+  /// Print to an ostream
+  ostream& print(ostream& os) const
+  {
+    return os<<"x";
+  }
+  
+  /// Returns the parameters
+  set<size_t> getParIds() const
+  {
+    return {};
+  };
+};
+
+/////////////////////////////////////////////////////////////////
+
 /// Parameter
 struct parameter_node_t : public node_t
 {
   /// Reference in the par list
-  const int id;
+  const size_t id;
   
   /// Creates taking the id
-  parameter_node_t(int id) : id(id)
+  parameter_node_t(size_t id) : id(id)
   {
   }
   
@@ -39,11 +84,25 @@ struct parameter_node_t : public node_t
   }
   
   /// Evaluate
-  double eval() const
+  double eval(const vector<double>& pars,const double& x) const
   {
     return pars[id];
   }
+  
+  /// Print to an ostream
+  ostream& print(ostream& os) const
+  {
+    return os<<"["<<id<<"]";
+  }
+  
+  /// Returns the parameters
+  set<size_t> getParIds() const
+  {
+    return {id};
+  };
 };
+
+/////////////////////////////////////////////////////////////////
 
 /// Real number
 struct real_node_t : public node_t
@@ -62,11 +121,25 @@ struct real_node_t : public node_t
   }
   
   /// Evaluate
-  double eval() const
+  double eval(const vector<double>& pars,const double& x) const
   {
     return val;
   }
+  
+  /// Print to an ostream
+  ostream& print(ostream& os) const
+  {
+    return os<<val;
+  }
+  
+  /// Returns the parameters
+  set<size_t> getParIds() const
+  {
+    return {};
+  };
 };
+
+/////////////////////////////////////////////////////////////////
 
 /// Unary expression
 struct uexp_node_t : public node_t
@@ -74,11 +147,14 @@ struct uexp_node_t : public node_t
   /// Function to be called
   double (*fun)(const double);
   
+  /// Full name
+  const string name;
+  
   /// Argument of the function
   const node_t* arg;
   
   /// Creates taking the function and the argument
-  uexp_node_t(double (*fun)(const double),const node_t* arg) : fun(fun),arg(arg)
+  uexp_node_t(double (*fun)(const double),const string& name,const node_t* arg) : fun(fun),name(name),arg(arg)
   {
   }
   
@@ -89,17 +165,37 @@ struct uexp_node_t : public node_t
   }
   
   /// Evaluate
-  double eval() const
+  double eval(const vector<double>& pars,const double& x) const
   {
-    return fun(arg->eval());
+    return fun(arg->eval(pars,x));
   }
+  
+  /// Print to an ostream
+  ostream& print(ostream& os) const
+  {
+    ostringstream oss;
+    arg->print(oss);
+    
+    return os<<combine(name.c_str(),oss.str().c_str());
+  }
+  
+  /// Returns the parameters
+  set<size_t> getParIds() const
+  {
+    return arg->getParIds();
+  };
 };
+
+/////////////////////////////////////////////////////////////////
 
 /// Binary expression
 struct bexp_node_t : public node_t
 {
   /// Function to be called
   double (*fun)(const double,const double);
+  
+  /// Full name of the function
+  const string name;
   
   /// First argument of the function
   const node_t* arg1;
@@ -108,7 +204,7 @@ struct bexp_node_t : public node_t
   const node_t* arg2;
   
   /// Creates taking the function and the argument
-  bexp_node_t(double (*fun)(const double,const double),const node_t* arg1,const node_t* arg2) : fun(fun),arg1(arg1),arg2(arg2)
+  bexp_node_t(double (*fun)(const double,const double),const string& name,const node_t* arg1,const node_t* arg2) : fun(fun),name(name),arg1(arg1),arg2(arg2)
   {
   }
   
@@ -120,10 +216,36 @@ struct bexp_node_t : public node_t
   }
   
   /// Evaluate
-  double eval() const
+  double eval(const vector<double>& pars,const double& x) const
   {
-    return fun(arg1->eval(),arg2->eval());
+    return fun(arg1->eval(pars,x),arg2->eval(pars,x));
   }
+  
+  /// Print to an ostream
+  ostream& print(ostream& os) const
+  {
+    ostringstream oss1,oss2;
+    arg1->print(oss1);
+    arg2->print(oss2);
+    
+    return os<<combine(name.c_str(),oss1.str().c_str(),oss2.str().c_str());
+  }
+  
+  /// Returns the parameters
+  set<size_t> getParIds() const
+  {
+    set<size_t> res=arg1->getParIds();
+    for(auto& i : arg2->getParIds())
+      res.insert(i);
+    
+    return res;
+  };
 };
+
+/// Print to an ostream
+inline ostream& operator<<(ostream& os,const node_t* node)
+{
+  return node->print(os);
+}
 
 #endif
