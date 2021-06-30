ca431469982674969c6c51988424af7eaa1af16f
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2021-07-01 01:00:38 +0200

    Reenlarged nconfs

--- projects/GM2/gm2.cpp
+++ projects/GM2/gm2.cpp
@@ -1,7 +1,7 @@
 #include <tranalisi.hpp>
 
 const size_t T=128;
-const size_t nconfs=28;
+const size_t nconfs=196;
 const index_t id({{"r2",2},{"r1",2},{"corr",61},{"T",T},{"ri",2}});
 
 std::pair<vector<double>,vector<double>> read(const char* path)
