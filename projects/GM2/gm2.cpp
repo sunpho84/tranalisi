8bd95cf749789ce571f13850e3e33e14842594f5
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2021-06-30 23:56:17 +0200

    Adding out

--- projects/GM2/gm2.cpp
+++ projects/GM2/gm2.cpp
@@ -1,8 +1,9 @@
 #include <tranalisi.hpp>
 
-const int T=128;
+const size_t T=128;
+const index_t id({{"r2",2},{"r1",2},{"corr",61},{"T",T}});
 
-vector<double> read(const char* path)
+std::pair<vector<double>,vector<double>> read(const char* path)
 {
   ifstream input(path);
   
@@ -20,21 +21,41 @@ vector<double> read(const char* path)
 	data.push_back(d);
     }
   
-  return data;
+  if(data.size()!=id.max())
+    CRASH("mismatch, %zu %zu",data.size(),id.max());
+  
+  vector<double> VV(T/2+1,0.0),PP(T/2+1,0.0);
+  for(size_t r1=0;r1<2;r1++)
+    //for(size_t r2=0;r2<2;r2++)
+      for(size_t _t=0;_t<T;_t++)
+	{
+	  const size_t t=std::min(T-_t,_t);
+	  
+	  const size_t r2=r1;
+	  for(size_t corr : {35,36,37})
+	    VV[t]+=data[id({r2,r1,corr,_t})];
+	  PP[t]+=data[id({r2,r1,5,_t})];
+	}
+  
+  for(auto& pp : PP)
+    pp/=2*2;
+  
+  for(auto& vv : VV)
+    vv/=2*2*3;
+  
+  return {PP,VV};
 }
-
-index_t id({{"r2",2},{"r1",2},{"corr",61},{"T",T}});
 
 int main(int narg,char** arg)
 {
   if(narg<2)
     CRASH("use %s file",arg[0]);
   
-  vector<double> data=read(arg[1]);
-  if(data.size()!=id.max())
-    CRASH("mismatch, %zu %zu",data.size(),id.max());
+  vector<double> PP,VV;
+  tie(PP,VV)=read(arg[1]);
   
-  //vector<double> VV
+  for(auto& pp : PP)
+    cout<<pp<<endl;
   
   // for(const double& d : data)
   //   cout<<d<<endl;
