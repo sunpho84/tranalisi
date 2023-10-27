04e8e637d202fe0e78cfbb2be35f2ef36ce8b6ec
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2023-10-27 16:42:08 +0200

    Adding a debug test on cov

--- projects/GILBACCO/loadCorrCov.cpp
+++ projects/GILBACCO/loadCorrCov.cpp
@@ -43,6 +43,8 @@ auto loadCorrCov(const string& path,
   res.clusterize((double)nConfs/njacks);
   
   /// Covariance matrix
+  // vector<double> naiveCovMatr((T/2+1)*(T/2+1),0.0);
+  // vector<double> naiveCorrMatr((T/2+1)*(T/2+1),0.0);
   vector<double> covMatr((T/2+1)*(T/2+1),0.0);
   
   for(size_t t=0;t<=T/2;t++)
@@ -70,7 +72,94 @@ auto loadCorrCov(const string& path,
 	covMatr[s+(T/2+1)*t]=
 	  covMatr[t+(T/2+1)*s]=
 	  sxy/sqrt(sxx*syy)*res[s].err()*res[t].err();
+	// naiveCorrMatr[s+(T/2+1)*t]=
+	//   naiveCorrMatr[t+(T/2+1)*s]=
+	//   sxy;
+	// naiveCovMatr[s+(T/2+1)*t]=
+	//   naiveCovMatr[t+(T/2+1)*s]=
+	//   sxy/sqrt(sxx*syy);
       }
+  
+  for(size_t t=0;t<=T/2;t++)
+    for(size_t s=0;s<=T/2;s++)
+      // covMatr[s+(T/2+1)*t]=naiveCovMatr[s+(T/2+1)*t]*res[s].err()*res[t].err();
+  
+  // {
+  
+  // if constexpr(not preciseCov)
+  //   {
+  //     for(size_t t=0;t<=T/2;t++)
+  // 	for(size_t s=0;s<=T/2;s++)
+  // 	  covMatr[t+(T/2+1)*s]=cov(res[t],res[s]);
+  //   }
+  // else
+  //   {
+
+  //   for(size_t nBoots=100;nBoots<100000;nBoots*=2)
+  //     {
+  //   gen_t<ranlux48> gen(91219392);
+  
+  // // const size_t nBoots=100000;
+  
+  // vector<double> ave(T/2+1,0.0);
+  // vector<double> c((T/2+1)*(T/2+1));
+  // vector<double> b(T/2+1,0.0);
+  
+  // for(auto& ci : c)
+  //   ci=0.0;
+  
+  // for(size_t iBoot=0;iBoot<nBoots;iBoot++)
+  //   {
+  //     for(auto& bi : b)
+  // 	bi=0.0;
+      
+  //     for(size_t iConf=0;iConf<nConfs;iConf++)
+  // 	{
+  // 	  const size_t i=gen.get_int(0,nConfs);
+  // 	  for(size_t t=0;t<=T/2;t++)
+  // 	    b[t]+=raw[iRaw({i,t})];
+  // 	}
+      
+  //     for(size_t t=0;t<=T/2;t++)
+  // 	b[t]/=nConfs;
+      
+  //     for(size_t t=0;t<=T/2;t++)
+  // 	{
+  // 	  ave[t]+=b[t];
+  // 	  for(size_t s=0;s<=T/2;s++)
+  // 	    c[s+(T/2+1)*t]+=b[t]*b[s];
+  // 	}
+  //   }
+  
+  // for(size_t t=0;t<=T/2;t++)
+  //   {
+  //     ave[t]/=nBoots;
+  //     for(size_t s=0;s<=T/2;s++)
+  // 	c[s+(T/2+1)*t]/=nBoots;
+  //   }
+  
+  // for(size_t t=0;t<=T/2;t++)
+  //   for(size_t s=0;s<=T/2;s++)
+  //     c[t+(T/2+1)*s]=(c[t+(T/2+1)*s]-ave[s]*ave[t])*nConfs;
+  
+  // // for(size_t t=0;t<=T/2;t++)
+  // //   {
+  // //     const double f=sqrt(c[t+(T/2+1)*t]);
+  // //     for(size_t s=0;s<=T/2;s++)
+  // // 	{
+  // // 	  c[t+(T/2+1)*s]/=f;
+  // // 	  c[s+(T/2+1)*t]/=f;
+  // // 	}
+  // //   }
+  
+  // // for(size_t t=0;t<=T/2;t++)
+  // //   for(size_t s=t;s<=T/2;s++)
+  // size_t t=1,s=2;
+  
+  // cout<<"AAA "<<nBoots<<" "<<c[t+(T/2+1)*s]<<" "<<naiveCorrMatr[t+(T/2+1)*s]<<endl;
+  // }
+  // }
+  // exit(1);
   
   return make_tuple(T,res,covMatr);
 }
