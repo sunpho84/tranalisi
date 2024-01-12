9ff83620c897a70d58480fef477131cc69f13b2f
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2024-01-12 11:20:31 +0100

    Adding parity -2

--- tools/djvec_load.cpp
+++ tools/djvec_load.cpp
@@ -8,13 +8,13 @@
 int main(int narg,char **arg)
 {
   //pars
-  int T=-1,ext_njacks=-1,iel=0;
+  int T=-1,ext_njacks=-1,iel=0,parity=-2;
   bool append=false;
   string bin_path="";
   
   //parse opts
   int c;
-  while((c=getopt(narg,arg,"T:ab:n:i::"))!= -1)
+  while((c=getopt(narg,arg,"T:ab:n:p:i::"))!= -1)
     {
       printf("%c\n",c);
       switch (c)
@@ -23,6 +23,7 @@ int main(int narg,char **arg)
       case 'a': append=true; break;
       case 'b': bin_path=optarg;break;
       case 'n': ext_njacks=to_int(optarg); break;
+      case 'p': parity=to_int(optarg); break;
       case 'i': iel=to_int(optarg); break;
       case '?': exit(0);break;
       default: CRASH("Unknown option -%c",optopt);
@@ -40,7 +41,7 @@ int main(int narg,char **arg)
       if(i-optind==1) path_out=arg[i];
     }
   
-  if(T==-1 or ext_njacks==-1 or path_in=="") close_with_mess("Use: %s -T=size -n=njacks -i=iel[0] path_in[stdin] -a -b=bin_path path_out[stdout]",arg[0]);
+  if(T==-1 or ext_njacks==-1 or path_in=="") close_with_mess("Use: %s -T=size -n=njacks -i=iel[0] path_in[stdin] -a -p=parity -b=bin_path path_out[stdout]",arg[0]);
   
   //put a warning
   if(path_in=="/dev/stdin") cerr<<"Reading from stdin"<<endl;
@@ -48,6 +49,9 @@ int main(int narg,char **arg)
   //set njacks and read
   set_njacks(ext_njacks);
   djvec_t data=read_djvec(path_in,T,iel);
+  
+  if(parity!=-2)
+    data=data.symmetrized(parity);
   
   //write average and error
   grace_file_t out(path_out);
--- tools/simple_effective_mass.cpp
+++ tools/simple_effective_mass.cpp
@@ -11,20 +11,41 @@ int main(int narg,char **arg)
   
   //pars
   int T=-1,par=1;
+  int tmin=-1,tmax=-1;
+  double mu=0;
   
   //parse opts
   int c;
-  while((c=getopt(narg,arg,"T:n:i::p::"))!= -1)
+  while((c=getopt(narg,arg,"T:m:M:n:i::u:p::"))!= -1)
     switch (c)
       {
-      case 'T': T=to_int(optarg); break;
-      case 'p': par=to_int(optarg); break;
-      case '?': exit(0);break;
+      case 'T':
+	T=to_int(optarg);
+	cout<<"parsing T="<<T<<endl;
+	break;
+      case 'm':
+	tmin=to_int(optarg);
+	cout<<"parsing tmin="<<tmin<<endl;
+	break;
+      case 'M':
+	tmax=to_int(optarg);
+	cout<<"parsing tmax="<<tmax<<endl;
+	break;
+      case 'u':
+	mu=strtod(optarg,nullptr);
+	cout<<"Parsing mu= "<<mu<<endl;
+	break;
+      case 'p':
+	par=to_int(optarg);
+	cout<<"Parsing par= "<<mu<<endl;
+	break;
       default: CRASH("Unknown option -%c",optopt);
       }
   
   //check mandatory options
   if(T==-1) cerr<<"Missing argument T"<<endl;
+  if(tmin==-1) cerr<<"Missing argument tmin"<<endl;
+  if(tmax==-1) cerr<<"Missing argument tmax"<<endl;
   
   //parse paths
   string path_in="/dev/stdin",path_out="/dev/stdout";
@@ -34,7 +55,7 @@ int main(int narg,char **arg)
       if(i-optind==1) path_out=arg[i];
     }
   
-  if(T==-1 or path_in=="") close_with_mess("Use: %s -T=size -p=par[1] path_in[stdin] path_out[stdout]",arg[0]);
+  if(T==-1 or path_in=="" or tmin==-1 or tmax==-1) close_with_mess("Use: %s -T=size -m=tmin -M=tmax -p=par[1] path_in[stdin] path_out[stdout]",arg[0]);
   
   //put a warning
   if(path_in=="/dev/stdin") cerr<<"Reading from stdin"<<endl;
@@ -45,11 +66,23 @@ int main(int narg,char **arg)
   if(!file_in.good()) CRASH("Opening %s to read",path_in.c_str());
   for(int t=0;t<T/2+1;t++)
     if(!(file_in>>ave[t]>>err[t]))
-      CRASH("Reading t=%zu",t);
+      CRASH("Reading t=%d",t);
     else
-      data[t].fill_gauss({ave[t],err[t],3224});
-      
-  effective_mass(data,T/2,par).ave_err().write(path_out);
+      data[t].fill_gauss({ave[t],err[t],3224+t});
+  
+  djack_t Z2,M;
+  two_pts_fit(Z2,M,data,T/2,tmin,tmax,path_out,"",par);
+  
+  cout<<"M: "<<M.ave_err()<<endl;
+  cout<<"Z: "<<sqrt(Z2).ave_err()<<endl;
+  
+  if(mu)
+    {
+      djack_t f=2*mu*sqrt(Z2)/(2*sqr(M));
+      cout<<"f: "<<f.ave_err()<<endl;
+    }
+  
+  // effective_mass(data,T/2,par).ave_err().write(path_out);
   
   return 0;
 }
