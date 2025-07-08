ae01d33dada07b1ceff5ab03053025d3ff758667
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2025-07-09 01:59:44 +0200

    Adding S

--- projects/DTOPI/dtopi.cpp
+++ projects/DTOPI/dtopi.cpp
@@ -7,6 +7,7 @@
 #include "math.hpp"
 #include "meas_vec.hpp"
 #include "obs_file.hpp"
+#include "oper.hpp"
 #include "raw_file.hpp"
 #include <filesystem>
 #include <string>
@@ -61,6 +62,16 @@ int main()
 	}
     }
   
+  auto get=[&data](const string& name,
+		   const bool reim) -> auto&
+  {
+    auto f=data.find(name);
+    if(f==data.end())
+      CRASH("unable to find %s",name.c_str());
+    
+    return f->second[reim];
+  };
+  
   {
     int i=0;
     for(const auto& [tag,dum] : data)
@@ -72,40 +83,24 @@ int main()
   }
   
 #define INS(A)				\
-  djvec_t& A=data.at(#A)[0];		\
+  djvec_t& A=get(#A,0);		\
   A.symmetrize()
   
   INS(Pi);
-  // INS(Pi_dth);
   INS(D);
   INS(D_sme);
   INS(D_sme_sme);
   
 #undef INS
   
-  // const double th=1e-5;
-  // const double kPi=M_PI/L*th;
-  // cout<<"k: "<<kPi<<endl;
-  
   djack_t ZPiLL,MPi;
   two_pts_fit(ZPiLL,MPi,Pi,T/2,20,30,"plots/Pi.xmg");
   const djack_t ZPiL=sqrt(ZPiLL);
   cout<<"MPi:     "<<smart_print(MPi)<<endl;
   
+  //aggiungere K, Ds
   const djack_t a=MPi/0.139;
   cout<<"a: "<<a<<endl;
-  
-  // const djack_t EPiExp=latt_en(MPi,kPi);
-  // cout<<"EPi exp: "<<smart_print(EPiExp)<<endl;
-  
-  // djack_t ZPiLL_dth,EPidTh;
-  // two_pts_fit(ZPiLL_dth,EPidTh,Pi_dth,T/2,20,30,"plots/Pi_dth.xmg");
-  // cout<<"EPi fit: "<<smart_print(EPidTh)<<endl;
-  
-  // const djack_t dEPi=EPidTh-MPi;
-  // cout<<"Epi-Mpi: "<<smart_print(dEPi)<<", significativity: "<<dEPi.significativity()<<endl;
-  
-  // (effective_mass(Pi_dth)-effective_mass(Pi)).ave_err().write("plots/pi_th_rat.xmg");
   
   djack_t ZDLL,ZDLS,ZDSS,MD;
   two_pts_fit(ZDLL,MD,D,T/2,20,25,"plots/D.xmg");
@@ -119,65 +114,80 @@ int main()
   cout<<"MD: "<<MD.ave_err()<<endl;
   
   const djack_t Q2Max=sqr(MD-MPi);
-  // const djack_t Q2=sqr(MD-EPidTh)-sqr(kPi);
   
   cout<<"Q2 max: "<<Q2Max.ave_err()<<endl;
-  // cout<<"Q2    : "<<Q2.ave_err()<<endl;
   
   const size_t dT=50;
   
 #define INS(A,B,C)							\
-  djvec_t A=data.at(#A)[B].subset(C,C+dT)*exp(MPi*C)/ZPiL/ZDS*MD*MPi*4;	\
+  djvec_t A=get(#A,B).subset(C,C+dT)*exp(MPi*C)/ZPiL/ZDS*MD*MPi*4;	\
   for(size_t t=0;t<A.size();t++)					\
     A[t]*=exp(MD*t);							\
   A.ave_err().write("plots/"#A".xmg")
   
-  INS(V0_20,0,20);
   INS(V0_30,0,30);
+  INS(V0_40,0,40);
   
-  INS(dth_V0_20,1,20);
-  INS(V1_20,1,20);
-  INS(dth_V1_20,0,20);
+  INS(S0_30,0,30);
+  INS(S0_40,0,40);
   
-  INS(dth_V0_30,1,30);
-  INS(V1_30,1,30);
-  INS(dth_V1_30,0,30);
+  // const djvec_t V0_5_DD=get("V0_5_DD",0);
+  // const djvec_t V0_10_DD=get("V0_10_DD",0);
   
-  const djvec_t V0_5_DD=data.at("V0_5_DD")[0];
-  const djvec_t V0_10_DD=data.at("V0_10_DD")[0];
+  // const djvec_t V0_30_PP=get("V0_30_PP",0);
+  // const djvec_t V0_40_PP=get("V0_40_PP",0);
   
-  const djvec_t V0_20_PP=data.at("V0_20_PP")[0];
-  const djvec_t V0_30_PP=data.at("V0_30_PP")[0];
-  
-  const auto cZ=
-    [](const djvec_t& in,
-       const djack_t& Z,
-       const djack_t& M,
-       const size_t& tw)
-    {
-      djvec_t out(T/2+1-tw);
-      for(size_t t=0;t<=T/2-tw;t++)
-	out[t]=two_pts_corr_fun(Z,M,T/2.0,t+tw,0)/in[t+tw];
+  // const auto cZ=
+  //   [](const djvec_t& in,
+  //      const djack_t& Z2,
+  //      const djack_t& M,
+  //      const size_t& tw)
+  //   {
+  //     djvec_t out(T/2+1-tw);
+  //     for(size_t t=0;t<=T/2-tw;t++)
+  // 	out[t]=Z2/(2*M)*exp(-M*(t+tw))/in[t+tw];
       
-      return out;
-    };
+  //     return out;
+  //   };
   
-  const djvec_t Z_5_DD=cZ(V0_5_DD,ZDSS,MD,5);
-  const djvec_t Z_10_DD=cZ(V0_10_DD,ZDSS,MD,10);
-  Z_5_DD.ave_err().write("plots/Z_5_DD.xmg");
-  Z_10_DD.ave_err().write("plots/Z_10_DD.xmg");
+  // const djvec_t Z_5_DD=cZ(V0_5_DD,ZDSS,MD,5);
+  // const djvec_t Z_10_DD=cZ(V0_10_DD,ZDSS,MD,10);
+  // Z_5_DD.ave_err().write("plots/Z_5_DD.xmg");
+  // Z_10_DD.ave_err().write("plots/Z_10_DD.xmg");
   
-  const djvec_t Z_20_PP=cZ(V0_20_PP,ZPiLL,MPi,20);
-  const djvec_t Z_30_PP=cZ(V0_30_PP,ZPiLL,MPi,30);
-  Z_20_PP.ave_err().write("plots/Z_20_PP.xmg");
-  Z_30_PP.ave_err().write("plots/Z_30_PP.xmg");
+  // const djvec_t Z_30_PP=cZ(V0_30_PP,ZPiLL,MPi,30);
+  // const djvec_t Z_40_PP=cZ(V0_40_PP,ZPiLL,MPi,40);
+  // Z_30_PP.ave_err().write("plots/Z_30_PP.xmg");
+  // Z_40_PP.ave_err().write("plots/Z_40_PP.xmg");
   
-  const djack_t Z=Z_20_PP[5];
+  // const djack_t Z=Z_30_PP[5];
+  // cout<<"Z: "<<smart_print(Z)<<endl;
   
-  const djvec_t f0_20=V0_20*Z/(MD+MPi);
-  const djvec_t f0_30=V0_30*Z/(MD+MPi);
-  f0_20.ave_err().write("plots/f0_20.xmg");
+  const djvec_t f0_30=S0_30*(0.231567-0.00072)/(sqr(MD)-sqr(MPi));
+  const djvec_t f0_40=S0_40*(0.231567-0.00072)/(sqr(MD)-sqr(MPi));
   f0_30.ave_err().write("plots/f0_30.xmg");
+  f0_40.ave_err().write("plots/f0_40.xmg");
+  
+  const djvec_t Z_30=S0_30/V0_30*(0.231567-0.00072)/(MD-MPi);
+  const djvec_t Z_40=S0_40/V0_40*(0.231567-0.00072)/(MD-MPi);
+  Z_30.ave_err().write("plots/Z_30.xmg");
+  Z_40.ave_err().write("plots/Z_40.xmg");
+  
+  INS(dth_V1_30,0,30);
+  INS(dth_V1_40,0,40);
+  
+  const djvec_t fp_30_Pi_D=(V0_30+(MD-MPi)*dth_V1_30)*Z_30/(2*MD);
+  const djvec_t fp_40_Pi_D=(V0_40+(MD-MPi)*dth_V1_40)*Z_40/(2*MD);
+  fp_30_Pi_D.ave_err().write("plots/fp_30_Pi_D.xmg");
+  fp_40_Pi_D.ave_err().write("plots/fp_40_Pi_D.xmg");
+  
+  INS(V1_dth_30,0,30);
+  INS(V1_dth_40,0,40);
+  
+  const djvec_t fp_30_D_Pi=(V0_30+(MPi-MD)*V1_dth_30)*Z_30/(2*MPi);
+  const djvec_t fp_40_D_Pi=(V0_40+(MPi-MD)*V1_dth_40)*Z_40/(2*MPi);
+  fp_30_D_Pi.ave_err().write("plots/fp_30_D_Pi.xmg");
+  fp_40_D_Pi.ave_err().write("plots/fp_40_D_Pi.xmg");
   
   return 0;
 }
