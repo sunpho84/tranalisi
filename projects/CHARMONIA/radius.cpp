a7a650228751e2e48d8309954ec20553845d1850
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2026-03-05 14:57:49 +0100

    Added analysis

--- projects/CHARMONIA/radius.cpp
+++ projects/CHARMONIA/radius.cpp
@@ -2,6 +2,7 @@
 #include <tranalisi.hpp>
 
 const size_t T{128};
+const size_t tw{35};
 const size_t L{64};
 const size_t nIns{2};
 const size_t nConfs{400};
@@ -9,22 +10,16 @@ const size_t nCopies{2};
 const size_t nEpsIn{5};
 const std::string insName[2]{"2pt","3pt"};
 
-const size_t nCombo{7};
 const double eps[2]{0.05,0.2};
-const std::array<double,nCombo> coef{0,0.0,0.0,0.0,0.0,sqr(eps[0]),sqr(eps[1])};
-std::vector<std::vector<std::pair<size_t,double>>> coefCombo{{{{0,+1.0}},                     //0 order
-							      {{1,+1.0}},
-							      {{2,+1.0}},
-							      {{3,+1.0}},
-							      {{4,+1.0}},
-  							      {{1,+0.5},{2,+0.5},{0,-1.0}},    //1st der in eps2
-							      {{3,+0.5},{4,+0.5},{0,-1.0}}}};  //same
 
 const size_t tMin2pts=30;
 const size_t tMax2pts=40;
 
-const size_t tMin3pts=20;
-const size_t tMax3pts=30;
+const size_t tMin2ptsIns=35;
+const size_t tMax2ptsIns=45;
+
+const size_t tMin3pts=22;
+const size_t tMax3pts=37;
 
 int main()
 {
@@ -40,167 +35,245 @@ int main()
   
   set_njacks(50);
   
-  const index_t id{{{"iCombo",nCombo},{"ins",nIns}}};
+  const index_t id{{{"iEpsIn",nEpsIn},{"ins",nIns}}};
   
-  std::vector<djvec_t> data(id.max(),djvec_t(T));
-  for(size_t iCombo=0;iCombo<nCombo;iCombo++)
-    {
-      cout<<"Combo "<<iCombo<<": "<<endl;
-      for(const auto& [i,c] : coefCombo[iCombo])
-	cout<<"("<<i<<","<<c<<")"<<endl;
-      
+  std::vector<djvec_t> cData(id.max(),djvec_t(T));
+  for(size_t iEpsIn=0;iEpsIn<nEpsIn;iEpsIn++)
       for(size_t iIns=0;iIns<nIns;iIns++)
 	{
-	  djvec_t& dout=data[id({iCombo,iIns})];
+	  djvec_t& dout=cData[id({iEpsIn,iIns})];
+	  dout=0.0;
 	  jackknivesFill(nConfs,
 			 [&](const size_t& iConf,
 			     const size_t& iClust,
 			     const double& w)
 			 {
-			   for(size_t iCopy=0;iCopy<nCopies;iCopy++)
-			     for(size_t t=0;t<T;t++)
-			       {
-				 double s{0.0};
-				 for(const auto& [i,c] : coefCombo[iCombo])
-				   {
-				     const size_t iLine=rawId({i,iIns,iConf,iCopy,t});
-				     if(iLine>=rawId.max())
-				       CRASH("unable to read i %lu, iIns %zu, iConf %zu, iCopy %zu, t %zu",i,iIns,iConf,iCopy,t);
-				     s+=c*rawData[iLine];
-				   }
-				 dout[t][iClust]+=w*s;
-			       }
+			   for(size_t t=0;t<T;t++)
+			     {
+			       double s=0.0;
+			       for(size_t iCopy=0;iCopy<nCopies;iCopy++)
+				 {
+				   const size_t iLine=rawId({iEpsIn,iIns,iConf,iCopy,t});
+				   if(iLine>=rawId.max())
+				     CRASH("unable to read iEpsIn %lu, iIns %zu, iConf %zu, iCopy %zu, t %zu",iEpsIn,iIns,iConf,iCopy,t);
+				   s+=w*rawData[iLine];
+				 }
+			       dout[t][iClust]+=s;
+			     }
 			 });
 	  dout.clusterize((double)nConfs/njacks);
 	  dout/=nCopies;
-	  if(coef[iCombo])
-	    dout/=coef[iCombo];
 	  
-	  dout.ave_err().write("plots/"+insName[iIns]+std::to_string(iCombo)+".xmg");
+	  dout.ave_err().write("plots/"+insName[iIns]+std::to_string(iEpsIn)+".xmg");
 	  //effective_mass(dout.subset(0,T/2),T/2,(iIns==0)?1:0).ave_err().write("plots/"+insName[iIns]+std::to_string(eps[iEps])+".xmg");
 	  
 	}
+  
+  const auto c2ptFun=
+    [&cData,
+     &id](const size_t iEps) -> djvec_t
+    {
+      return cData[id({iEps,0})].symmetrized();
+    };
+  
+  for(size_t iEps=0;iEps<nEpsIn;iEps++)
+    c2ptFun(iEps).ave_err().write("plots/c2pts"+std::to_string(iEps)+".xmg");
+  
+  const auto c3ptFun=
+    [&cData,
+     &id](const size_t& iEps)
+    {
+      return cData[id({iEps,1})].subset(tw,T/2);
+    };
+  
+  for(size_t iEps=0;iEps<nEpsIn;iEps++)
+    c3ptFun(iEps).ave_err().write("plots/c3pts"+std::to_string(iEps)+".xmg");
+  
+  /////////////////////////////////////////////////////////////////
+  
+  const djvec_t c2pt=c2ptFun(0);
+  const djvec_t eff_mass=effective_mass(c2pt,T/2);
+  const djvec_t eff_sq_coupling=effective_squared_coupling(c2pt,eff_mass,T/2);
+  
+  const djack_t myM=constant_fit(eff_mass,tMin2pts,tMax2pts,"plots/c2ptEffmass.xmg");
+  
+  /////////////////////////////////////////////////////////////////
+  
+  /// Finite eps analysis
+  cout<<"---- Finite eps analysis ---"<<endl;
+  
+  djack_t Z2[nEpsIn],E[nEpsIn];
+  djack_t F[nEpsIn];
+  djvec_t ss[nEpsIn];
+  for(size_t iEps=0;iEps<nEpsIn;iEps++)
+    {
+      const djvec_t c2=c2ptFun(iEps);
+      two_pts_fit(Z2[iEps],E[iEps],c2,T/2,tMin2pts,tMax2pts,"plots/m"+to_string(iEps)+".xmg");
+      const djvec_t eff_massE=effective_mass(c2,T/2);
+      const djvec_t eff_sq_couplingE=effective_squared_coupling(c2,eff_massE,T/2);
+      
+      djvec_t cF(T/2-tw);
+      ss[iEps].resize(T/2-tw);
+      const djvec_t c3=c3ptFun(iEps);
+      for(size_t t=0;t<T/2-tw;t++)
+	{
+	  const djack_t m=E[0];
+	  const djack_t e=E[iEps];
+	  const djack_t z20=Z2[0];
+	  const djack_t z2=Z2[iEps];
+	  // const djack_t m=eff_mass[t+tw];
+	  // const djack_t e=eff_massE[t+tw];
+	  // const djack_t z20=eff_sq_coupling[t+tw];
+	  // const djack_t z2=eff_sq_couplingE[t+tw];
+	  
+	  ss[iEps][t]=e*c3[t]/sqrt(z2)/(m+e)
+	    /exp(-e*tw)/exp(-m*t);
+	  
+	  cF[t]=4*m*e*c3[t]/sqrt(z20*z2)/(m+e)
+	    /exp(-e*tw)/exp(-m*t);
+	}
+      F[iEps]=constant_fit(cF,tMin3pts,tMax3pts,"plots/F"+to_string(iEps)+".xmg");
+      ss[iEps].ave_err().write("plots/ss"+to_string(iEps)+".xmg");
     }
   
-  // {
-  //   cout.precision(16);
-  //   const djack_t E1=constant_fit(effective_mass(data[id({3,0})].symmetrized()),30,35,"plots/E1.xmg");
-  //   const djack_t E2=constant_fit(effective_mass(data[id({4,0})].symmetrized()),30,35,"plots/E2.xmg");
-  //   const djack_t M=constant_fit(effective_mass(data[id({0,0})].symmetrized()),30,35,"plots/M.xmg");
-  //   djack_t SL;
-  //   {
-  //     djack_t Z2,DZ2_fr_Z2,M;
-  //     two_pts_with_ins_ratio_fit(Z2,M,DZ2_fr_Z2,SL,data[id({0,0})].symmetrized(),data[id({5,0})].symmetrized(),T/2,30,40,"plots/t0.xmg","plots/t1.xmg");
-  //     cout<<"SL from 2nd der in eps2: "<<SL.ave_err()<<endl;
-  //   }
-  //   cout<<"M: "<<M.ave_err()<<endl;
-  //   cout<<"E1: "<<E1.ave_err()<<endl;
-  //   cout<<"E2: "<<E2.ave_err()<<endl;
-  //   djack_t Ee1=latt_en_1D(M,M_PI/L*0.05);
-  //   cout<<"Ee1: "<<Ee1.ave_err()<<endl;
-  //   const djack_t t1=(E1-M)/(sqr(0.05));
-  //   const djack_t t2=(E2-M)/(sqr(0.05));
-  //   cout<<t1.ave_err()<<endl;
-  //   cout<<t2.ave_err()<<endl;
-  //   cout<<smart_print((Ee1-M)/(sqr(0.05)))<<endl;
-  // }
+  djvec_t Q2(2);
+  for(size_t iEst=0;iEst<2;iEst++)
+    Q2[iEst]=sqr(E[0]-E[2*iEst+1])-sqr(M_PI*eps[iEst]/L);
   
-  const djvec_t c2pt=data[id({0,0})].symmetrized();
-  djack_t Z2,M;
-  djvec_t DZ2_fr_Z2(2),SL(2);
   for(size_t iEst=0;iEst<2;iEst++)
     {
-      const djvec_t c2ptIns=data[id({iEst+nEpsIn,0})].symmetrized();
-      two_pts_with_ins_ratio_fit(Z2,M,DZ2_fr_Z2[iEst],SL[iEst],c2pt,c2ptIns,T/2,tMin2pts,tMax2pts,"plots/c2pt0.xmg","plots/c2pt"+std::to_string(iEst+nEpsIn)+".xmg");
-      effective_slope(c2ptIns/c2pt,effective_mass(c2ptIns),T/2).ave_err().write("plots/c2ptSlope"+std::to_string(iEst+nEpsIn)+".xmg");
+      const djvec_t s=(ss[2*iEst+1]+ss[2*iEst+2])/(2*ss[0])-1;
+      (s/Q2[iEst]).ave_err().write("plots/s"+to_string(iEst)+".xmg");
     }
+  
+  const djack_t& M=E[0];
   cout<<"M: "<<smart_print(M)<<endl;
+  
+  const djack_t a=M/2.9839;
+  cout<<"a: "<<smart_print(a)<<" GeV^-1"<<endl;
+  
+  djack_t r2[2];
+  // djvec_t Q2G(2);
+  for(size_t iEst=0;iEst<2;iEst++)
+    {
+      //cout<<" "<<smart_print(F[2*iEps+1])<<" "<<smart_print(F[2*iEps+2])<<" "<<smart_print(F[0])<<endl;
+      const djack_t G=(F[2*iEst+1]+F[2*iEst+2])/(2*F[0])-1;
+      //cout<<"G: "<<smart_print(G)<<endl;
+      
+      const djack_t EG=sqrt(sqr(E[0])+sqr(eps[iEst]*M_PI/L));
+      // Q2G[iEst]=-2.0*E[0]*(EG-E[0]);
+      // cout<<"Q2 unc: "<<smart_print((Q2[iEst]-Q2G[iEst])*1e12)<<endl;
+      
+      r2[iEst]=6*G/Q2[iEst]*sqr(a);
+      cout<<"Eps "<<eps[iEst]<<": "<<smart_print(r2[iEst])<<endl;
+    }
+  
+  cout<<"diff: "<<smart_print(r2[1]-r2[0])<<endl;
+  
+  for(size_t iEst=0;iEst<2;iEst++)
+    {
+      const djack_t d=Q2[iEst]/(sqr(M_PI/L*eps[iEst]));
+      cout<<"Q2/p2: "<<d<<endl;
+    }
+  
+  /////////////////////////////////////////////////////////////////
+  
+  // Slope analysis
+  cout<<"--- Slope analysis ---"<<endl;
+  
+  const auto getCInsFun=
+    [](const auto& cFun)
+    {
+      return
+	[&cFun](const size_t& iEst)->djvec_t
+	{
+	  return (cFun(2*iEst+1)+cFun(2*iEst+2)-2*cFun(0))/(2*sqr(eps[iEst]));
+	};
+    };
+  
+  const auto c2ptInsFun=
+    getCInsFun(c2ptFun);
+  
+  for(size_t iEst=0;iEst<2;iEst++)
+    c2ptInsFun(iEst).ave_err().write("plots/c2ptsIns"+std::to_string(iEst)+".xmg");
+  
+  djvec_t DZ2_fr_Z2(2),SL(2);
+  djvec_t c2ptInsRat[2];
+  djvec_t eff_slope[2];
+  djvec_t eff_slope_offset[2];
+  
+  for(size_t iEst=0;iEst<2;iEst++)
+    {
+      c2ptInsRat[iEst]=c2ptInsFun(iEst)/c2pt;
+      eff_slope[iEst]=effective_slope(c2ptInsRat[iEst],eff_mass,T/2);
+      SL[iEst]=constant_fit(eff_slope[iEst],tMin2ptsIns,tMax2ptsIns,"plots/c2ptSlope"+std::to_string(iEst)+".xmg");
+      eff_slope_offset[iEst]=effective_squared_coupling_rel_corr(c2ptInsRat[iEst],eff_mass,eff_slope[iEst],T/2);
+      DZ2_fr_Z2[iEst]=constant_fit(eff_slope_offset[iEst],tMin2ptsIns,tMax2ptsIns,"plots/c2ptSlopeOffset"+std::to_string(iEst)+".xmg");
+    }
   cout<<"SL0: "<<smart_print(SL[0])<<endl;
   cout<<"SL1: "<<smart_print(SL[1])<<endl;
   
   const djack_t expSL=-sqr(M_PI/L)/(2*M);
   cout<<"expSL: "<<smart_print(expSL)<<endl;
   
-  const djack_t a=M/2.9839;
-  cout<<"a: "<<a.ave_err()<<endl;
+  const auto c3ptInsFun=getCInsFun(c3ptFun);
   
-  const size_t tw=35;
-  djvec_t c3pt=data[id({0,1})].subset(tw,T/2);
+  for(size_t iEst=0;iEst<2;iEst++)
+    c3ptInsFun(iEst).ave_err().write("plots/c3ptsIns"+std::to_string(iEst)+".xmg");
   
-  djvec_t czv(T/2+1-tw);
-  for(size_t it=0;it<czv.size();it++)
-    czv[it]=two_pts_corr_fun(Z2,M,T/2.0,tw+it,0)/c3pt[it];
-   const djack_t zv=constant_fit(czv,tMin3pts,tMax3pts,"plots/zv.xmg");
+  const djvec_t c3pt=c3ptFun(0);
   
   djvec_t dff[2];
-  djack_t D[2];
-  for(size_t iEps=0;iEps<2;iEps++)
+  djack_t R2[2];
+  for(size_t iEst=0;iEst<2;iEst++)
     {
-      djvec_t c3ptIns=data[id({iEps+nEpsIn,1})].subset(tw,T/2);
+      djvec_t c3ptIns=c3ptInsFun(iEst);
       const djvec_t r3pt=c3ptIns/c3pt;
-      r3pt.ave_err().write("plots/r3pt"+std::to_string(iEps)+".xmg");
+      r3pt.ave_err().write("plots/r3pt"+std::to_string(iEst)+".xmg");
       
-      dff[iEps]=r3pt;
-      for(size_t t=0;t<dff[iEps].size();t++)
+      
+      for(size_t t=0;t<T/2-tw;t++)
 	{
-	  dff[iEps][t]-=DZ2_fr_Z2[iEps]/2+SL[iEps]*(1/(2*M)+tw);
-	  dff[iEps][t]*=-6.0*sqr(L)/sqr(M_PI)*sqr(a);
+	  dff[iEst].resize(T/2-tw);
+	  const djack_t dz2_fr_z2=DZ2_fr_Z2[iEst];
+	  const djack_t sl=SL[iEst];
+	  const djack_t m=M;
+	  // const djack_t dz2_fr_z2=eff_slope_offset[iEst][t+tw];
+	  // const djack_t sl=eff_slope[iEst][t+tw];
+	  // const djack_t m=eff_mass[t+tw];
+	  dff[iEst][t]=r3pt[t];
+	  dff[iEst][t]-=dz2_fr_z2/2+sl*(1/(2*m)+tw);
+	  dff[iEst][t]*=-6.0*sqr(L)/sqr(M_PI)*sqr(a);
 	}
-      D[iEps]=constant_fit(dff[iEps],20,25,"plots/dff"+std::to_string(iEps)+".xmg");
-      cout<<"Eps "<<eps[iEps]<<" : "<<smart_print(D[iEps])<<endl;
+      R2[iEst]=constant_fit(dff[iEst],tMin3pts,tMax3pts,"plots/dff"+std::to_string(iEst)+".xmg");
+      cout<<"Eps "<<eps[iEst]<<" : "<<smart_print(R2[iEst])<<endl;
     }
   
-  djvec_t diff=dff[1]-dff[0];
+  const djvec_t diff=dff[1]-dff[0];
   diff.ave_err().write("plots/diff.xmg");
   
-  // for(size_t t=0;t<c3pt.size();t++)
-  //   c3pt[t]/=exp(-M*(tw+t))*Z2/M;
-  // c3pt.ave_err().write("plots/c3pt.xmg");
+  cout<<"diff: "<<smart_print(R2[1]-R2[0])<<endl;
   
   /////////////////////////////////////////////////////////////////
   
-  {
-    djack_t Z2[nEpsIn],E[nEpsIn];
-    djack_t F[nEpsIn];
-    for(size_t iEps=0;iEps<nEpsIn;iEps++)
-      {
-	two_pts_fit(Z2[iEps],E[iEps],data[id({iEps,0})].symmetrized(),T/2,tMin2pts,tMax2pts,"plots/m"+to_string(iEps)+".xmg");
-	
-	djvec_t cF(T/2-tw+1);
-	for(size_t t=0;t<=T/2-tw;t++)
-	  cF[t]=4*E[0]*E[iEps]*data[id({iEps,1})][tw+t]/sqrt(Z2[0]*Z2[iEps])/(E[0]+E[iEps])
-	    /exp(-E[iEps]*tw)/exp(-E[0]*t);
-	
-	F[iEps]=constant_fit(cF,tMin3pts,tMax3pts,"plots/F"+to_string(iEps)+".xmg");
-      }
-    
-    djack_t dz2_fr_z2[2];
-    djack_t sl[2];
-    for(size_t iEps=0;iEps<2;iEps++)
-      {
-	dz2_fr_z2[iEps]=((Z2[2*iEps+1]+Z2[2*iEps+2])/(2*Z2[0])-1)/sqr(eps[iEps]);
-	cout<<"dz/z2: "<<smart_print(dz2_fr_z2[iEps])<<" vs "<<smart_print(DZ2_fr_Z2[iEps])<<endl;
-	
-	sl[iEps]=((E[2*iEps+1]+E[2*iEps+2])/2-E[0])/sqr(eps[iEps]);
-	cout<<"sl: "<<smart_print(sl[iEps])<<" vs "<<smart_print(SL[iEps])<<endl;
-      }
-    
-    djack_t r2[2];
-    for(size_t iEps=0;iEps<2;iEps++)
-      {
-	//cout<<" "<<smart_print(F[2*iEps+1])<<" "<<smart_print(F[2*iEps+2])<<" "<<smart_print(F[0])<<endl;
-	const djack_t G=(F[2*iEps+1]+F[2*iEps+2])/(2*F[0])-1;
-	//cout<<"G: "<<smart_print(G)<<endl;
-	
-	const djack_t Q2=sqr(E[0]-E[2*iEps+1])-sqr(M_PI*eps[iEps]/L);
-	//cout<<"Q2: "<<smart_print(Q2)<<endl;
-	
-	r2[iEps]=6*G/Q2*sqr(a);
-	cout<<"r2 "<<eps[iEps]<<": "<<smart_print(r2[iEps])<<endl;
-      }
-    
-    cout<<smart_print(r2[1]-r2[0])<<endl;
-  }
+  // Check on dZ2/Z2 ans sl
+  cout<<"--- Comparison of finite eps and slope ---"<<endl;
+  
+  djack_t dz2_fr_z2[2];
+  djack_t sl[2];
+  for(size_t iEps=0;iEps<2;iEps++)
+    {
+      dz2_fr_z2[iEps]=((Z2[2*iEps+1]+Z2[2*iEps+2])/(2*Z2[0])-1)/sqr(eps[iEps]);
+      cout<<"dz/z2: "<<smart_print(dz2_fr_z2[iEps])<<" vs "<<smart_print(DZ2_fr_Z2[iEps])<<endl;
+    }
+  
+  for(size_t iEps=0;iEps<2;iEps++)
+    {
+      sl[iEps]=((E[2*iEps+1]+E[2*iEps+2])/2-E[0])/sqr(eps[iEps]);
+      cout<<"sl: "<<smart_print(sl[iEps])<<" vs "<<smart_print(SL[iEps])<<endl;
+    }
+  
+  cout<<"m: "<<smart_print(M)<<" vs "<<smart_print(myM)<<endl;
   
   return 0;
 }
