f6d3b497f58edc3f57909c50f0f3d55870565b84
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2026-04-12 02:16:57 +0200

    Adding 6

--- projects/PIPI/smeRad.cpp
+++ projects/PIPI/smeRad.cpp
@@ -146,7 +146,7 @@ int main()
 
   const size_t tminLL=6;
   const size_t tminLS=6;
-  const size_t tminSS=5;
+  const size_t tminSS=6;
   add(tminLL,c[0],0,0,1,1);
   add(tminLS,c[1],0,2,1,3);
   add(tminSS,c[3],2,2,3,3);
