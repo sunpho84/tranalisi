a91c7f30d2a638244b9e348e376b84b0442bbe40
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2026-04-16 11:04:18 +0200

    adjusting guesses

--- projects/PIPI/smeRad.cpp
+++ projects/PIPI/smeRad.cpp
@@ -123,7 +123,7 @@ int main()
       jf.add_fit_par(ZS[1],"ZS1",guesses[3],0.001);
       jf.add_fit_par(M[0],"M0",guesses[4],0.01);
       jf.add_fit_par(M[1],"M1",guesses[5],0.01);
-      
+            
       auto f=
 	[](const auto& C,
 	   const auto& M,
@@ -211,14 +211,14 @@ int main()
   c.push_back(get("P3_SMP3_SR0_M3_0__M3_SMM3_SR0_P3_0__P5P5"));
   c.push_back(get("P3_SMP3_SR0_M3_0__M3_SMM3_SR0_P3_0__P5P5"));
   c.push_back(get("P3_SMP3_SR0_SMP3_M3_0__M3_SMM3_SR0_SMM3_P3_0__P5P5"));
-  tsFit(c,{0.19,0.23,0.01,0.01,0.18,0.57},"SL",{6,6,5},20);
+  tsFit(c,{0.176,0.218,0.014,0.0067,0.23,0.57},"SL",{6,6,5},20);
   
   std::vector<djvec_t> d;
   d.push_back(get("P3_SMP3_SR0_SMP3_M3_0__M3_SMM3_SR0_SMM3_P3_0__P5P5"));
   d.push_back(-get("P3_SMP3_SR0_SMP3_M3_0__M3_SMM3_SR0_SMM3_P3_0__A0P5"));
   d.push_back(-get("P3_SMP3_SR0_SMP3_M3_0__M3_SMM3_SR0_SMM3_P3_0__A0P5"));
   d.push_back(-get("P3_SMP3_SR0_SMP3_M3_0__M3_SMM3_SR0_SMM3_P3_0__A0A0"));
-  tsFit(d,{0.014,0.006,-0.004,0.006,0.23,0.58},"AP",{5,5,5},16);
+  tsFit(d,{-0.014,-0.0036,-0.006,0.006,0.23,0.58},"AP",{5,5,5},16);
   
   return 0;
 }
