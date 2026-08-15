053cf89ebbe4df23cd829369c0d3820159a8fc07
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2026-08-16 00:54:18 +0200

    Adding debug

--- projects/PIPI/pipi.cpp
+++ projects/PIPI/pipi.cpp
@@ -722,7 +722,9 @@ std::vector<djvec_t> computeDirect(const index_t& idOut)
 	      //cout<<a<<" "<<b<<" "<<momSo[2]<<" "<<momSi[2]<<endl;
 	      
 	      const size_t iCombo=(momSo!=momSi);
-	      
+
+	      size_t i=idOut({iBSo,iBSi,iCombo});
+	      cout<<" "<<i<<" "<<res.size()<<endl;
 	      res[idOut({iBSo,iBSi,iCombo})]+=getDirect(a,b)[0]*momSo[2]*momSi[2];
 	      
 	      iSi++;
